173ab68ec35d1307ed19fc478f956bbe1c08766b
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-02 13:47:44 -0300

    add gameLoop in tsGame2d

--- src/TsGame2D/TsGame2D.ts
+++ src/TsGame2D/TsGame2D.ts
@@ -3,10 +3,12 @@ import DisplayHandler from '../Handlers/DisplayHandler'
 import InterfaceTsGame2D from './InterfaceTsGame2D'
 import Context2D from '../Context2D'
 import Keyboard from '../KeyBoard'
+import GameLoop from '../GameLoop'
 
 export default class TsGame2D implements InterfaceTsGame2D {
   public readonly display: DisplayHandler = new DisplayHandler()
   private readonly context: Context2D = new Context2D(this.display.screen)
   public readonly draw: Drawing = new Drawing(this.context.ref)
   public readonly keyBoard: Keyboard = new Keyboard()
+  public readonly gameLoop: GameLoop= new GameLoop()
 }
