446779dff2f35b67144f8786e0a54b89c406f84f
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-03 13:05:59 -0300

    create player

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,54 @@
 import TsGame2D from "./TsGame2D";
+import Shapes from "./Types/Shapes";
 
 const tsg = new TsGame2D();
 
 const loop = tsg.gameLoop;
 loop.init();
 
-loop.onUpdate = () => {};
+const background = tsg.draw.rect({
+  x: 0,
+  y: 0,
+  ...tsg.display.getSize(),
+  color: "black",
+  type: "fill",
+});
+
+const player = tsg.draw.rect({
+  x: 10,
+  y: 10,
+  width: 30,
+  height: 30,
+  color: "blue",
+  type: "fill",
+});
+
+const playerSpeed = { x: 5, y: 5 };
+
+const wall = tsg.draw.rect({
+  x: 250,
+  y: 100,
+  width: 30,
+  height: 100,
+  color: "grey",
+  type: "fill",
+});
+
+function playerMoviment(player: Shapes.TypeRect) {
+  const { press } = tsg.keyBoard;
+
+  if (press.check("d")) player.x += playerSpeed.x;
+  if (press.check("a")) player.x -= playerSpeed.x;
+  if (press.check("w")) player.y -= playerSpeed.y;
+  if (press.check("s")) player.y += playerSpeed.y;
+}
+
+loop.onUpdate = () => {
+  playerMoviment(player);
+};
+
+loop.onRender = () => {
+  tsg.draw.rect(background);
+  tsg.draw.rect(player);
+  tsg.draw.rect(wall);
+};
