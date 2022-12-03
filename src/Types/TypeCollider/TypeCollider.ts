d1c4284347fdcf4ab0c6d05205adcf90641de84b
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-03 13:31:41 -0300

    ajust

--- src/Types/TypeCollider/TypeCollider.ts
+++ src/Types/TypeCollider/TypeCollider.ts
@@ -1,8 +1,26 @@
 import TypeSize from "../TypeSize";
 import TypeAxis from "../TypeAxis";
 namespace TypeCollider {
-  type BlockedRectSolid = TypeSize.Size & TypeAxis.Axis;
-  type BlockerRectSolid = TypeAxis.Axis & TypeAxis.Axis;
+  export type BlockedRectSolid = TypeSize.Size & TypeAxis.Axis;
+  export type BlockerRectSolid = TypeSize.Size & TypeAxis.Axis;
+
+  export type BlockedRectSolidInX = Omit<
+    TypeCollider.BlockedRectSolid,
+    "y" | "height"
+  >;
+  export type BlockerRectSolidInX = Omit<
+    TypeCollider.BlockerRectSolid,
+    "y" | "height"
+  >;
+
+  export type BlockedRectSolidInY = Omit<
+    TypeCollider.BlockedRectSolid,
+    "x" | "width"
+  >;
+  export type BlockerRectSolidInY = Omit<
+    TypeCollider.BlockerRectSolid,
+    "x" | "width"
+  >;
 }
 
 export default TypeCollider;
