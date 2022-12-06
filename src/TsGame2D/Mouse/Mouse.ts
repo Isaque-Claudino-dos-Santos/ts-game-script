0d9c3993f7a75bb645ad60f26bbebeec508569db
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-06 12:41:45 -0300

    create method click

--- src/TsGame2D/Mouse/Mouse.ts
+++ src/TsGame2D/Mouse/Mouse.ts
@@ -4,11 +4,14 @@ import TypeMouse from "../Types/TypeMouse";
 export default class Mouse implements InterfaceMouse {
   public point: TypeMouse.Point = { x: 0, y: 0 };
   public isDown: boolean = false;
+  public click: boolean = false;
   public button: number;
+
   constructor(private readonly elementRef: HTMLElement) {
     this.elementRef.onmousedown = (event: MouseEvent) => {
       this.button = event.button;
       this.isDown = true;
+      this.click = true;
     };
 
     this.elementRef.onmouseup = (event: MouseEvent) => {
@@ -21,5 +24,16 @@ export default class Mouse implements InterfaceMouse {
       this.point.x = x;
       this.point.y = y;
     };
+  }
+
+  onClick(
+    callback: (point: TypeMouse.Point) => void,
+    button: keyof TypeMouse.ButtonByID = "0"
+  ): void {
+    if (this.click && this.isDown && this.button === Number(button))
+      callback(this.point);
+
+    this.click = false;
+    this.button = undefined;
   }
 }
