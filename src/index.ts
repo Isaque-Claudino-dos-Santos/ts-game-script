ee83f6ac4629fac794bd0683a16462167c552c1e
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-06 00:23:18 -0300

    use player and background

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,20 @@
-import tsg from "./TsGame2D";
+import TsGame2D from "./TsGame2D";
+import Player from "./GameObjects/Player";
+import Background from "./GameObjects/Background";
+
+const tsg: TsGame2D = new TsGame2D();
 
 const loop = tsg.gameLoop;
-
 loop.init();
 
-const background = tsg.draw.rect({
-  ...tsg.display.getSize(),
-  x: 0,
-  y: 0,
-  color: "black",
-});
-
-const player = tsg.draw.rect({
-  x: 10,
-  y: 10,
-  width: 30,
-  height: 30,
-  color: "blue",
-});
-
-const playerNick = tsg.draw.text({
-  text: "isaque",
-  color: "white",
-  size: 16,
-});
-
-function playerMoviment() {
-  const press = tsg.keyBoard.press;
-  const vel = 2;
-  if (press.check("d")) player.x += vel;
-  if (press.check("a")) player.x -= vel;
-  if (press.check("s")) player.y += vel;
-  if (press.check("w")) player.y -= vel;
-}
+const background = new Background(tsg);
+const player = new Player(tsg);
 
 loop.onUpdate = () => {
-  playerMoviment();
-  playerNick.relativeWith(player, player.width / 2, -2);
+  player.update();
 };
 
 loop.onRender = () => {
   background.render();
   player.render();
-  playerNick.render();
 };
