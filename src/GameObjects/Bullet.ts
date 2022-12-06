22bffc07c8ae4c728e2e248742bf8bc5429dab21
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-06 18:11:34 -0300

    implement bullet

--- src/GameObjects/Bullet.ts
+++ src/GameObjects/Bullet.ts
@@ -1,10 +1,11 @@
 import TsGame2D from "../TsGame2D";
 import DrawArc from "../TsGame2D/Drawing/DrawArc";
+import Mesure from "../TsGame2D/Mesure";
 export default class Bullet {
   public readonly draw: DrawArc;
-  public catX: number;
-  public catY: number;
-
+  public angle: number;
+  public velocity: number = 15;
+  public slot: number;
   constructor(private tsg: TsGame2D) {
     this.draw = this.tsg.draw.arc({
       color: "purple",
@@ -15,7 +16,23 @@ export default class Bullet {
   }
 
   public goToTarget() {
-    //inplement go to target    
+    this.draw.x -= Math.cos(Mesure.toRadian(this.angle)) * this.velocity;
+    this.draw.y -= Math.sin(Mesure.toRadian(this.angle)) * this.velocity;
+  }
+
+  private destroy(localStorege: Bullet[]) {
+    delete localStorege[this.slot];
+  }
+
+  public ruleToDestroy(localStorege: Bullet[]) {
+    if (
+      this.draw.x < 0 ||
+      this.draw.x > this.tsg.display.width ||
+      this.draw.y < 0 ||
+      this.draw.y > this.tsg.display.height
+    ) {
+      this.destroy(localStorege);
+    }
   }
 
   public render(): void {
