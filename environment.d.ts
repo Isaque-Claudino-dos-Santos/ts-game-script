a9430de0f5b54ac6becd704a4dac3b1e0ace9050
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-10 17:35:07 -0300

    add vas

--- environment.d.ts
+++ environment.d.ts
@@ -1,11 +1,14 @@
 declare global {
-    namespace NodeJS {
-      interface ProcessEnv {
+  namespace NodeJS {
+    interface ProcessEnv {
       readonly CANVAS_ELEMENT_ID: string;
       readonly CANVAS_WIDTH: string;
       readonly CANVAS_HEIGHT: string;
+      readonly APP_DIR_SRC: string;
+      readonly APP_FILE_ENTRY: string;
+      readonly APP_DIR_GAME_OBJECT: string;
     }
   }
 }
 
-export {}
+export {};
