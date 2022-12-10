7c94b3573c00c15c56f63bbda5727df332806bd4
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-10 13:41:10 -0300

    reset

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,11 @@
 import TsGame2D from "./TsGame2D";
-import Player from "./GameObjects/Player";
-import Background from "./GameObjects/Background";
-import Enimy from "./GameObjects/Enimy";
-import Wall from "./GameObjects/Wall";
 
 const tsg: TsGame2D = new TsGame2D();
 
 const loop = tsg.gameLoop;
 
-const background = new Background(tsg);
-const enimy = new Enimy(tsg);
-const player = new Player(tsg,enimy);
-const wall = new  Wall(tsg)
+loop.onUpdate = () => {};
 
-loop.onUpdate = () => {
-  player.update();
-  enimy.update();
-  tsg.collider.rect.solid(player.draw,wall.draw)
-};
-
-loop.onRender = () => {
-  background.render();
-  player.render();
-  enimy.render();
-  wall.render()
-};
+loop.onRender = () => {};
 
 loop.init();
