25e9225ae3f49aa6f8e6d30174290b6f26a0283a
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-11 12:52:12 -0300

    add comand and create and validate

--- vendor/TsGameCommands/Commands/CommandCreate/CommandCreate.ts
+++ vendor/TsGameCommands/Commands/CommandCreate/CommandCreate.ts
@@ -2,11 +2,19 @@ import fs from 'fs';
 import { env } from 'process';
 import TypeCommand from '../../Types/TypeCommand';
 import path from 'path';
-
+import captalize from '../../../captalize';
 export default class CommandCreate {
   object(args: TypeCommand.Flags) {
+    if (!args.name) {
+      console.log('Name object not defined'.red);
+      console.log('Use flag'.green, 'name=examplaName'.blue);
+      throw '';
+    }
+
+    if (!args.draw) args.draw = 'rect';
+
     const dir: string = path.resolve(env.APP_DIR_GAME_OBJECT ?? '');
-    const file: string = `${dir}/${args.name}.ts`;
+    const file: string = `${dir}/${captalize(args.name)}.ts`;
     const pathFileModule: string = './vendor/TsGameCommands/resources/modules';
 
     if (!fs.existsSync(dir)) fs.mkdirSync(dir);
@@ -14,23 +22,26 @@ export default class CommandCreate {
     if (!fs.existsSync(file))
       fs.open(file, 'w', (err, f) => {
         if (err) console.log(err);
-
-        console.log(`File ${args.name}.ts create`);
+        console.log(`File ${captalize(args.name)}.ts not exist `.yellow);
+        console.log(`File ${captalize(args.name)}.ts created`.green);
       });
 
     fs.readFile(pathFileModule + '/gameObject.txt', (err, data) => {
       if (err) console.log(err);
 
+      console.log('Prepare to write in file!'.yellow);
       let code: string = data.toString();
-      code = code.replace(/VAR_CLASS_NAME/g, args.name);
-      code = code.replace(
-        /VAR_TYPE_DRAW/g,
-        `Draw${args.draw.charAt(0).toLocaleUpperCase() + args.draw.slice(1)}`
-      );
+      code = code.replace(/VAR_CLASS_NAME/g, captalize(args.name));
+      console.log('add class name', captalize(args.name).blue);
+      code = code.replace(/VAR_TYPE_DRAW/g, `Draw${captalize(args.draw)}`);
+      console.log('add type draw', `Draw${captalize(args.draw)}`.blue);
       code = code.replace(/VAR_METHOD_DRAW/g, args.draw);
+      console.log('add method draw', args.draw.blue, '\n');
 
       fs.writeFile(file, code, (err) => {
         if (err) console.log(err);
+
+        console.log('Game Object Create with success (:'.green, '\n');
       });
     });
   }
