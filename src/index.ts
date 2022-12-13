2ebdfa42af4a301f57eaef40f238ba5094bc4d65
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-13 13:03:58 -0300

    use init method

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import TsGame2D from '../vendor/TsGame2D';
-import Home from './Rooms/Menu';
+import Menu from './Rooms/Menu';
 import Arena from './Rooms/Arena';
 import Player from './Objects/Player';
 
 const tsg: TsGame2D = new TsGame2D();
-
-const player = new Player(tsg);
-
-const home = new Home(tsg);
-const arena = new Arena(tsg, player);
 
 tsg.init();
