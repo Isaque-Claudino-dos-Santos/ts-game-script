a08439bf4c7fc237ad5ce89d6782ce29ca9f180e
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-13 13:45:52 -0300

    pass to arrow function

--- vendor/TsGame2D/KeyBoard/KeyBoardClick/KeyBoardClick.ts
+++ vendor/TsGame2D/KeyBoard/KeyBoardClick/KeyBoardClick.ts
@@ -1,14 +1,14 @@
-import InterfaceKeyBoardClick from "./InterfaceKeyBoardClick";
+import InterfaceKeyBoardClick from './InterfaceKeyBoardClick';
 
 export default class KeyBoardClick implements InterfaceKeyBoardClick {
   public key: string;
-  public isDown: boolean;
+  public isDown: boolean = false;
   public active: boolean;
 
-  check(key: string): boolean {
+  check = (key: string) => {
     if (!this.isDown || key !== this.key) return false;
     this.active = true;
     this.isDown = false;
     return true;
-  }
+  };
 }
