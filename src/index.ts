12e77006942a521f8508ec511094e60f1f789ab3
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-13 13:16:12 -0300

    set up

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import TsGame2D from '../vendor/TsGame2D';
-import Menu from './Rooms/Menu';
-import Arena from './Rooms/Arena';
-import Player from './Objects/Player';
 
 const tsg: TsGame2D = new TsGame2D();
 
