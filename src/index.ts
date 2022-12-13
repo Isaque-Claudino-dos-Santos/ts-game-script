e284a564244a990454410bd2768d8bf5ea5ed59d
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-13 12:59:52 -0300

    test init

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,10 @@ import Arena from './Rooms/Arena';
 import Player from './Objects/Player';
 
 const tsg: TsGame2D = new TsGame2D();
-const loop = tsg.gameLoop;
 
 const player = new Player(tsg);
 
 const home = new Home(tsg);
 const arena = new Arena(tsg, player);
 
-loop.onUpdate = () => tsg.room.callUpdate();
-
-loop.onRender = () => tsg.room.callRender();
-
-loop.init();
+tsg.init();
