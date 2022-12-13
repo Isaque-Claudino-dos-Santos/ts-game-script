37fce0554d4b735aaabbf8ce2f2b54f6cf44fd08
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-13 13:55:20 -0300

    set snake in  room

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import TsGame2D from '../vendor/TsGame2D';
+import RoomMain from './Rooms/RoomMain';
+import Snake from './Objects/Snake';
 
 const tsg: TsGame2D = new TsGame2D();
-
 tsg.init();
+
+tsg.display.setSize({
+  width: 400,
+  height: 400,
+});
+
+const size = tsg.display.width / 40;
+
+const snake = new Snake(tsg, size);
+
+new RoomMain(tsg, snake);
