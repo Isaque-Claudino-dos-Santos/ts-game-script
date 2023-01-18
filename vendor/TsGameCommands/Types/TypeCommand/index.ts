a98eca4d06d2bc7b2591a5e1e17369050da09488
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-01-17 21:56:01 -0300

    mv file and remove command

--- vendor/TsGameCommands/Types/TypeCommand/index.ts
+++
@@ -1,2 +0,0 @@
-import TypeCommand from './TypeCommand';
-export default TypeCommand;
--- vendor/captalize.ts
+++
@@ -1,3 +0,0 @@
-export default function captalize(str: string): string {
-  return str.charAt(0).toLocaleUpperCase() + str.slice(1);
-}
