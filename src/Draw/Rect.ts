ce30ff6dedcfe006514f81fc860275ce3d4f6262
isaque claudino <isaqueclaudino12@gmail.com>
2023-03-21 21:05:31 -0300

    create class to render rect

---
+++ src/Draw/Rect.ts
@@ -0,0 +1,37 @@
+export default class Rect {
+    x: number = 0
+    y: number = 0
+    width: number = 0
+    height: number = 0
+    color: string = 'black'
+    rotate: number = 0
+
+    constructor(public context: CanvasRenderingContext2D) { }
+
+    public reposition(x: number, y: number) {
+        this.x = x
+        this.y = y
+    }
+
+    public resize(width: number, height: number) {
+        this.width = width
+        this.height = height
+    }
+
+    public render(type: 'fill' | 'stroke' = 'fill') {
+        this.context.save()
+        this.context.translate(
+            this.x + this.width / 2,
+            this.y + this.height / 2
+        )
+        this.context.rotate(this.rotate)
+        this.context.fillStyle = this.color
+        this.context[`${type}Rect`](
+            this.x + -this.width / 2,
+            this.y + -this.height / 2,
+            this.width,
+            this.height
+        )
+        this.context.restore()
+    }
+}
