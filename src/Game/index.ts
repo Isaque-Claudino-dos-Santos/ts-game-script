da88892df0f885a9b5b48250a58228b6b896da09
isaque claudino <isaqueclaudino12@gmail.com>
2023-03-21 22:30:49 -0300

    style code

--- src/Game/index.ts
+++ src/Game/index.ts
@@ -1,9 +1,9 @@
 import Canvas from '@Display/Canvas'
 import Rect from '@Draw/Rect'
-
 const canvas = new Canvas(600, 400)
 
 const r1 = new Rect(canvas.context)
 r1.reposition(10, 10)
 r1.resize(30, 30)
 r1.render()
+
