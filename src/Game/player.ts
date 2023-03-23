7b906e7d03923c0ff55abb97483969497fc7a33e
isaque claudino <isaqueclaudino12@gmail.com>
2023-03-23 09:58:21 -0300

    code style

--- src/Game/player.ts
+++ src/Game/player.ts
@@ -4,12 +4,10 @@ import Loop from '@Display/Loop'
 import Keyborad from '@Event/Keyboard'
 
 const player = new Rect(context)
-
 const speed = {
   x: 5,
   y: 5
 }
-
 const playerLoop = new Loop()
 const playerKey = new Keyborad()
 
@@ -22,7 +20,6 @@ playerKey.create('KeyW', () => player.y -= speed.y)
 player.reposition(10, 10)
 player.resize(30, 30)
 player.color = '#ff55aa'
-
 
 playerLoop.onUpdate = () => {
   playerKey.checkPress('KeyD')
