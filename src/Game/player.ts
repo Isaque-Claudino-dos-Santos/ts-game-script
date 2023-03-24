1596f42ad41ae049528306eea3c0c10bbac44ba0
isaque claudino <isaqueclaudino12@gmail.com>
2023-03-24 18:41:59 -0300

    test gravity

--- src/Game/player.ts
+++ src/Game/player.ts
@@ -2,6 +2,8 @@ import { context } from './canvas'
 import Rect from '@Draw/Rect'
 import Loop from '@Display/Loop'
 import Keyborad from '@Event/Keyboard'
+import Gravity from '@Physical/Gravity'
+
 
 const player = new Rect(context)
 const speed = {
@@ -10,6 +12,7 @@ const speed = {
 }
 const playerLoop = new Loop()
 const playerKey = new Keyborad()
+const playerGravity = new Gravity(player)
 
 player.reposition(10, 10)
 player.resize(30, 30)
@@ -18,7 +21,10 @@ player.color = '#ff55aa'
 playerKey.create('KeyD', () => player.x += speed.x)
 playerKey.create('KeyA', () => player.x -= speed.x)
 playerKey.create('KeyS', () => player.y += speed.y)
-playerKey.create('KeyW', () => player.y -= speed.y)
+playerKey.create('KeyW', () => {
+  player.y -= speed.y
+  playerGravity.reset()
+})
 playerKey.create('KeyQ', () => player.rotate -= 1 * Math.PI / 180)
 playerKey.create('KeyE', () => player.rotate += 1 * Math.PI / 180)
 playerKey.create('ControlLeft', () => player.color = player.color === '#dddddd' ? '#ff55aa' : '#dddddd')
@@ -31,6 +37,7 @@ playerLoop.onUpdate = () => {
   playerKey.checkPress('KeyQ')
   playerKey.checkPress('KeyE')
   playerKey.checkClick('ControlLeft')
+  playerGravity.vertical()
 }
 
 playerLoop.onRender = () => {
