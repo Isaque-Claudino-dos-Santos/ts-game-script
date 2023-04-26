e806b24ab9f2b52dfb1032d04c4185775187ecd4
isaque <isaqueclaudino12@gmail.com>
2023-04-26 00:37:02 -0300

    ajust

--- src/Draw/Rect.ts
+++ src/Draw/Rect.ts
@@ -1,41 +1,38 @@
 export default class Rect {
-    x: number = 0
-    y: number = 0
-    width: number = 0
-    height: number = 0
-    color: string = 'black'
-    rotate: number = 0
+  x: number = 0
+  y: number = 0
+  width: number = 0
+  height: number = 0
+  color: string = 'black'
+  rotate: number = 0
 
-    constructor(private readonly context: CanvasRenderingContext2D) { }
+  constructor(private readonly context: CanvasRenderingContext2D) {}
 
-    public reposition(x: number, y: number) {
-        this.x = x
-        this.y = y
-    }
+  public reposition(x: number, y: number) {
+    this.x = x
+    this.y = y
+  }
 
-    public resize(width: number, height: number) {
-        this.width = width
-        this.height = height
-    }
+  public resize(width: number, height: number) {
+    this.width = width
+    this.height = height
+  }
 
-    public render(type: 'fill' | 'stroke' = 'fill') {
-        this.context.save()
-        this.context.translate(
-            this.x + this.width / 2,
-            this.y + this.height / 2
-        )
-        this.context.rotate(this.rotate)
-        this.context.fillStyle = this.color
-        this.context[`${type}Rect`](
-            -this.width / 2,
-            -this.height / 2,
-            this.width,
-            this.height
-        )
-        this.context.restore()
-    }
+  public render(type: 'fill' | 'stroke' = 'fill') {
+    this.context.save()
+    this.context.translate(this.x + this.width / 2, this.y + this.height / 2)
+    this.context.rotate(this.rotate)
+    this.context.fillStyle = this.color
+    this.context[`${type}Rect`](
+      -this.width / 2,
+      -this.height / 2,
+      this.width,
+      this.height
+    )
+    this.context.restore()
+  }
 
-    public log() {
-        console.log(this)
-    }
+  public log() {
+    console.log(this)
+  }
 }
