f166142228b844e1f1f9a74076f26734c854b3dd
isaque <isaqueclaudino12@gmail.com>
2023-04-28 22:24:11 -0300

    code styled

--- src/Display/Canvas.ts
+++ src/Display/Canvas.ts
@@ -1,20 +1,24 @@
 export default class Canvas {
-  public readonly screen: HTMLCanvasElement;
+  public readonly screen: HTMLCanvasElement
   public readonly context: CanvasRenderingContext2D
 
   constructor(width: number = 300, height: number = 300) {
-    this.screen = document.querySelector('canvas') ?? document.createElement('canvas')
+    this.screen =
+      document.querySelector('canvas') ?? document.createElement('canvas')
     this.screen.width = width
     this.screen.height = height
     this.context = this.getCanvasContext2D(this.screen)
   }
 
-
-  private getCanvasContext2D(canvas: HTMLCanvasElement): CanvasRenderingContext2D {
-    const context = canvas.getContext('2d');
+  private getCanvasContext2D(
+    canvas: HTMLCanvasElement
+  ): CanvasRenderingContext2D {
+    const context = canvas.getContext('2d')
     if (!context) {
       throw console.error(
-        new Error('Error in set Context: canvas element not valid or not defined')
+        new Error(
+          'Error in set Context: canvas element not valid or not defined'
+        )
       )
     }
     return context
