083f88b1266174838bf4c277149d06644e816ed6
isaque <isaqueclaudino12@gmail.com>
2023-04-29 02:29:02 -0300

    add mouse event in player

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import Canvas from '@Display/Canvas'
 import Rect from '@Draw/Rect'
 import KeyBoard from '@Event/Keyboard'
 import Loop from '@Event/Loop'
+import Mouse, { TypeMouse } from '@Event/Mouse'
 
 const { context, screen } = new Canvas(600, 450)
 const keyboard = new KeyBoard()
+const mouse = new Mouse(screen)
 const loop = new Loop()
 
 const background = new Rect(context)
@@ -16,11 +18,32 @@ const player = new Rect(context)
   .reposition(30, 30)
   .setColor('#aa00ff')
 
+const playerMouse = {
+  isDown: false,
+  onDown() {
+    this.isDown = true
+  },
+  onUp() {
+    this.isDown = false
+  },
+}
+
+const backgroundMouse = {
+  onMove(mouse: TypeMouse) {
+    if (!playerMouse.isDown) return
+    player.x = mouse.x - player.width / 2
+    player.y = mouse.y - player.height / 2
+  },
+}
+
 keyboard
   .create('KeyD', () => (player.x += 4))
   .create('KeyS', () => (player.y += 4))
   .create('KeyW', () => (player.y -= 4))
   .create('KeyA', () => (player.x -= 4))
+
+mouse.addEvents(background, 0, backgroundMouse)
+mouse.addEvents(player, 0, playerMouse)
 
 loop.onUpdate = () => {
   keyboard
