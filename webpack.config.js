9c647e940a21c9ccf4471b2e8ae64c281d5d886f
isaque <isaqueclaudino12@gmail.com>
2023-04-29 20:43:09 -0300

    add alias

--- webpack.config.js
+++ webpack.config.js
@@ -7,60 +7,61 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const isProduction = process.env.NODE_ENV == 'production'
 
 const config = {
-    entry: {
-        main: './src/index.ts',
-    },
-    output: {
-        filename: '[name].js',
-        chunkFilename: '[name].chunk.js',
-        path: path.resolve(__dirname, 'build'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'public/index.html',
-        }),
-        new dotenv(),
+  entry: {
+    main: './src/index.ts',
+  },
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+    new dotenv(),
 
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: 'ts-loader',
+        exclude: ['/node_modules/'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|svg|webp)$/i,
+        type: 'asset/resource',
+        use: [{ loader: 'file-loader' }],
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/i,
-                loader: 'ts-loader',
-                exclude: ['/node_modules/'],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|svg|webp)$/i,
-                type: 'asset/resource',
-                use: [{ loader: 'file-loader' }],
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+    alias: {
+      '@Draw': path.resolve(__dirname, 'src', 'Draw'),
+      '@Display': path.resolve(__dirname, 'src', 'Display'),
+      '@Event': path.resolve(__dirname, 'src', 'Event'),
+      '@Physical': path.resolve(__dirname, 'src', 'Physical'),
+      Game$: path.resolve(__dirname, 'src', 'Game', 'index.ts'),
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
-        alias: {
-            '@Draw': path.resolve(__dirname, 'src', 'Draw'),
-            '@Display': path.resolve(__dirname, 'src', 'Display'),
-            '@Event': path.resolve(__dirname, 'src', 'Event'),
-            '@Physical': path.resolve(__dirname, 'src', 'Physical')
-        }
-    },
+  },
 }
 
 module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production'
-    } else {
-        config.mode = 'development'
-    }
-    return config
+  if (isProduction) {
+    config.mode = 'production'
+  } else {
+    config.mode = 'development'
+  }
+  return config
 }
