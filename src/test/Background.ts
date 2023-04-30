23cdef9baed3769d8a2c503db7cc32e574e5929b
isaque <isaqueclaudino12@gmail.com>
2023-04-29 22:55:13 -0300

    add player

--- src/test/Background.ts
+++ src/test/Background.ts
@@ -1,8 +1,12 @@
 import Rect from '@Draw/Rect'
 import GameObject from '@Game/GameObject'
 import Game from 'Game'
+import Player from './Player'
+import { TypeMouseActions } from '@Event/Mouse'
 
 export default class Background extends GameObject {
+  public player?: Player
+
   constructor(game: Game) {
     super(game)
   }
@@ -10,6 +14,22 @@ export default class Background extends GameObject {
   body = new Rect(this.game.canvas.context)
     .resize(this.game.canvas.screen.width, this.game.canvas.screen.height)
     .setColor('#232323')
+
+  public mouse0: TypeMouseActions = {
+    onUp: () => {
+      if (!this.player) return
+      this.player.isDown = false
+    },
+    onMove: (mouse) => {
+      if (!this.player || !this.player.isDown) return
+      this.player.body.x = mouse.x - this.player.body.width / 2
+      this.player.body.y = mouse.y - this.player.body.height / 2
+    },
+  }
+
+  public boot = () => {
+    this.game.mouse.addEvents(this.body, 0, this.mouse0)
+  }
 
   public render = () => {
     this.body.render()
