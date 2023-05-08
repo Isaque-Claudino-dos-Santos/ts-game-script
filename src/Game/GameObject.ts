f9fe4c0399ecbe9c9f9904aaf3ce197f3db61514
isaque <isaqueclaudino12@gmail.com>
2023-05-08 19:58:02 -0300

    fixed code not utils

--- src/Game/GameObject.ts
+++ src/Game/GameObject.ts
@@ -1,22 +1,9 @@
 import Rect from '@Draw/Rect'
-import KeyBoard from '@Event/Keyboard'
-import { TypeMouseActions } from '@Event/Mouse'
-import Game from 'Game'
 
 type TypeBody = Rect
 
-export default abstract class GameObject {
-  public abstract body: TypeBody
-  protected readonly keyboard: KeyBoard
-  public mouse0: TypeMouseActions = {}
-  public mouse1: TypeMouseActions = {}
-  public mouse2: TypeMouseActions = {}
-  public mouse3: TypeMouseActions = {}
-  public mouse4: TypeMouseActions = {}
-
-  constructor(protected readonly game: Game) {
-    this.keyboard = game.keyboard()
-  }
+export default class GameObject {
+  public body!: TypeBody
 
   /**
    * @toOverrite
