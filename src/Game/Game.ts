931014fe1ba970b973e23893b0c0f1a588760c68
isaque <isaqueclaudino12@gmail.com>
2023-05-08 19:57:30 -0300

    add createGameObject in Game class

--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -2,13 +2,39 @@ import Canvas from '@Display/Canvas'
 import KeyBoard from '@Event/Keyboard'
 import Loop from '@Event/Loop'
 import Mouse from '@Event/Mouse'
+import GameObject from './GameObject'
 
 export default class Game {
   public readonly canvas = new Canvas(600, 400)
   public readonly mouse = new Mouse(this.canvas.screen)
-  public readonly loop = new Loop()
+  private readonly loop = new Loop()
+
+  private gameObjects: GameObject[] = []
+
+  constructor() {
+    this.handleGameObjects()
+  }
+
+  private handleGameObjects() {
+    this.gameObjects.forEach((gameObject) => gameObject.boot())
+
+    this.loop.onUpdate = () =>
+      this.gameObjects.forEach((gameObject) => gameObject.update())
+    this.loop.onRender = () =>
+      this.gameObjects.forEach((gameObject) => gameObject.render())
+
+    this.loop.init()
+  }
 
   public keyboard(): KeyBoard {
     return new KeyBoard()
+  }
+
+  public createGameObject(
+    callback: (gameObject: GameObject, game: Game) => void
+  ) {
+    const gameObject = new GameObject()
+    callback(gameObject, this)
+    this.gameObjects.unshift(gameObject)
   }
 }
