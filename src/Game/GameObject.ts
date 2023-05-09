946149b1884d89474c99c1b08a2000c79562a273
isaque <isaqueclaudino12@gmail.com>
2023-05-09 00:01:04 -0300

    created array with render,update and boot  methods

--- src/Game/GameObject.ts
+++ src/Game/GameObject.ts
@@ -1,23 +1,22 @@
 import Rect from '@Draw/Rect'
 
 type TypeBody = Rect
+type TypeFn = () => void
+type TypeFnArray = TypeFn[]
 
 export default class GameObject {
   public body!: TypeBody
+  public readonly renders: TypeFnArray = []
+  public readonly boots: TypeFnArray = []
+  public readonly updates: TypeFnArray = []
 
-  /**
-   * @toOverrite
-   * @arrowFunction
-   */
-  public boot() {}
-  /**
-   * @toOverrite
-   * @arrowFunction
-   */
-  public update() {}
-  /**
-   * @toOverrite
-   * @arrowFunction
-   **/
-  public render() {}
+  public boot(callback: TypeFn) {
+    this.boots.push(callback)
+  }
+  public render(callback: TypeFn) {
+    this.renders.push(callback)
+  }
+  public update(callback: TypeFn) {
+    this.updates.push(callback)
+  }
 }
