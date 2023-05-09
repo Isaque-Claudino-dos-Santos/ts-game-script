48da4e1613f21c2fcddfb620fb0a7aeabeb39fdb
isaque <isaqueclaudino12@gmail.com>
2023-05-09 00:57:11 -0300

    testing gameObject instances of Handler

--- src/test/index.ts
+++ src/test/index.ts
@@ -2,30 +2,29 @@ import Game from 'Game'
 import Rect from '@Draw/Rect'
 
 const game = new Game()
-const keyboard = game.keyboard()
+const { gameObject } = game
 const { context, screen } = game.canvas
 
-const playerID = game.createGameObject((player, game) => {
-  const {
-    canvas: { context },
-    keyboard,
-  } = game
-
+const playerID = gameObject.create((player, game) => {
   player.body = new Rect(context)
     .resize(30, 30)
     .reposition(40, 40)
     .setColor('#88ff')
 
-  const key = keyboard().create('Space', () =>
-    player.body.setColor('red' === player.body.color ? 'blue ' : 'red')
+  player.keyboard.create('Space', () =>
+    player.body.setColor('#99dddd' === player.body.color ? 'white ' : '#99dddd')
   )
+
+  player.update(() => {
+    player.keyboard.checkClick('Space')
+  })
 
   player.render(() => {
     player.body.render()
   })
 })
 
-const bgID = game.createGameObject((background) => {
+const bgID = gameObject.create((background) => {
   background.body = new Rect(context)
     .resize(screen.width, screen.height)
     .setColor('#232323')
@@ -35,12 +34,13 @@ const bgID = game.createGameObject((background) => {
   })
 })
 
-game.socketGameObject([playerID, bgID], ([player, bg]) => {
-  keyboard.create('Space', () => (bg.body.color = '#0066aa'))
-
-  player.body.color = '#004422'
+gameObject.socket([playerID, bgID], ([player, bg]) => {
+  bg.keyboard.create(
+    'Space',
+    () => (bg.body.color = bg.body.color === '#232323' ? '#004422' : '#232323')
+  )
 
   player.update(() => {
-    keyboard.checkClick('Space')
+    bg.keyboard.checkClick('Space')
   })
 })
