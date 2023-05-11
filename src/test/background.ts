66dd97b83a7079f41f9af6e94fc02ffcf93efd9b
isaque <isaqueclaudino12@gmail.com>
2023-05-11 20:33:44 -0300

    created background

---
+++ src/test/background.ts
@@ -0,0 +1,19 @@
+import Rect from '@Draw/Rect'
+import { canvas, gameObject } from './game'
+
+const background = gameObject.create((background) => {
+  background.body = new Rect(canvas.context)
+    .resize(screen.width, screen.height)
+    .setColor('#232323')
+
+  background.keyboard.create('Space', () => {
+    background.body.color =
+      background.body.color === '#232323' ? '#004422' : '#232323'
+  })
+
+  background.render(() => {
+    background.body.render()
+  })
+}, 'background')
+
+export default background
