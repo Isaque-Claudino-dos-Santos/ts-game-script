eea11b80881a77466fe9a7f7a5244fccf8d94a8c
isaque <isaqueclaudino12@gmail.com>
2023-05-11 20:48:29 -0300

    created game object wall

---
+++ src/test/wall.ts
@@ -0,0 +1,23 @@
+import Rect from '@Draw/Rect'
+import { canvas, gameObject } from './game'
+
+const wall = gameObject.create((wall) => {
+  wall.body = new Rect(canvas.context)
+    .resize(30, 90)
+    .reposition(150, 100)
+    .setColor('#ffff')
+
+  wall.keyboard.create('Space', () =>
+    wall.body.setColor('#99dddd' === wall.body.color ? 'white ' : '#99dddd')
+  )
+
+  wall.update(() => {
+    wall.keyboard.checkClick('Space')
+  })
+
+  wall.render(() => {
+    wall.body.render()
+  })
+}, 'wall')
+
+export default wall
