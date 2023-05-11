3711ad394253be1499ab41b2f2884a9f7591a55e
isaque <isaqueclaudino12@gmail.com>
2023-05-11 20:24:18 -0300

    ajust

--- src/test/index.ts
+++ src/test/index.ts
@@ -29,17 +29,17 @@ const background = gameObject.create((background) => {
     .resize(screen.width, screen.height)
     .setColor('#232323')
 
+  background.keyboard.create('Space', () => {
+    background.body.color =
+      background.body.color === '#232323' ? '#004422' : '#232323'
+  })
+
   background.render(() => {
     background.body.render()
   })
 }, 'background')
 
 gameObject.save(background).save(player)
-
-background.keyboard.create('Space', () => {
-  background.body.color =
-    background.body.color === '#232323' ? '#004422' : '#232323'
-})
 
 player.update(() => {
   background.keyboard.checkClick('Space')
