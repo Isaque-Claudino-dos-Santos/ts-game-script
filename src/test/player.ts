0a44de502315691a760ec14a2e832618d8471127
isaque <isaqueclaudino12@gmail.com>
2023-05-11 20:48:20 -0300

    add  moviment in player

--- src/test/player.ts
+++ src/test/player.ts
@@ -7,12 +7,23 @@ const player = gameObject.create((player) => {
     .reposition(40, 40)
     .setColor('#88ff')
 
+  player.keyboard
+    .create('KeyW', () => (player.body.y -= 3))
+    .create('KeyS', () => (player.body.y += 3))
+    .create('KeyD', () => (player.body.x += 3))
+    .create('KeyA', () => (player.body.x -= 3))
+
   player.keyboard.create('Space', () =>
     player.body.setColor('#99dddd' === player.body.color ? 'white ' : '#99dddd')
   )
 
   player.update(() => {
-    player.keyboard.checkClick('Space')
+    player.keyboard
+      .checkClick('Space')
+      .checkPress('KeyW')
+      .checkPress('KeyS')
+      .checkPress('KeyD')
+      .checkPress('KeyA')
   })
 
   player.render(() => {
