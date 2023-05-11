f070cea640458c0efed6268323233a25c632ddc6
isaque <isaqueclaudino12@gmail.com>
2023-05-11 20:34:01 -0300

    import game contents

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,46 +1,11 @@
-import Game from 'Game'
-import Rect from '@Draw/Rect'
+import { game } from './game'
+import player from './player'
+import background from './background'
 
-const game = new Game()
-const { gameObject } = game
-const { context, screen } = game.canvas
-
-const player = gameObject.create((player) => {
-  player.body = new Rect(context)
-    .resize(30, 30)
-    .reposition(40, 40)
-    .setColor('#88ff')
-
-  player.keyboard.create('Space', () =>
-    player.body.setColor('#99dddd' === player.body.color ? 'white ' : '#99dddd')
-  )
-
-  player.update(() => {
-    player.keyboard.checkClick('Space')
-  })
-
-  player.render(() => {
-    player.body.render()
-  })
-}, 'player')
-
-const background = gameObject.create((background) => {
-  background.body = new Rect(context)
-    .resize(screen.width, screen.height)
-    .setColor('#232323')
-
-  background.keyboard.create('Space', () => {
-    background.body.color =
-      background.body.color === '#232323' ? '#004422' : '#232323'
-  })
-
-  background.render(() => {
-    background.body.render()
-  })
-}, 'background')
-
-gameObject.save(background).save(player)
+game.gameObject.save(background).save(player)
 
 player.update(() => {
   background.keyboard.checkClick('Space')
 })
+
+game.init()
