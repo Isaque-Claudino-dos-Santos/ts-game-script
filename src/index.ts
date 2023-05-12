6522ec06cc9c4369c21c4cdce07b9b1b8808e900
isaque <isaqueclaudino12@gmail.com>
2023-05-12 20:11:59 -0300

    resized canvas

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,8 @@ import PacMan from './PacMan/PacMan'
 
 const engine = new Engine(new Canvas2d())
 engine.game = new PacMan(engine)
+
+engine.window.screen.width = 600
+engine.window.screen.height = 450
+
 engine.init()
