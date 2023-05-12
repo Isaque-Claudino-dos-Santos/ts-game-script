bd56bfc5d6b21e51b44fe850a67e63f46f94372e
isaque <isaqueclaudino12@gmail.com>
2023-05-12 20:11:04 -0300

    implemented in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-import './test/index'
+import Canvas2d from '@Game/Canvas2d'
+import Engine from '@Engine/Engine'
+import PacMan from './PacMan/PacMan'
+
+const engine = new Engine(new Canvas2d())
+engine.game = new PacMan(engine)
+engine.init()
--- src/test/index.ts
+++
@@ -1,7 +0,0 @@
-import Canvas2d from '@Game/Canvas2d'
-import Engine from '@Engine/Engine'
-import PacMan from '../PacMan/PacMan'
-
-const engine = new Engine(new Canvas2d())
-engine.game = new PacMan(engine)
-engine.init()
