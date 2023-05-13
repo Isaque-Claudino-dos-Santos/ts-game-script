bbe362b830c7da58ba46a6bd16077c8cb8527fae
isaque <isaqueclaudino12@gmail.com>
2023-05-13 02:42:36 -0300

    implmented bounding box

--- src/Engine/BoundingBox.ts
+++ src/Engine/BoundingBox.ts
@@ -1,11 +1,25 @@
 import InterfaceBoundingBox from '@Interface/InterfaceBoundingBox'
 import TypeSprite from '@Type/TypeSprite'
+import Rect from './Draw/Rect'
+import Arc from './Draw/Arc'
 
 export default class BoundingBox implements InterfaceBoundingBox {
-  constructor(public shape: TypeSprite, public parent: TypeSprite) {}
+  constructor(public box: TypeSprite, public parent: TypeSprite) {}
 
-  update = () => {
-    this.shape.x = this.parent.x
-    this.shape.y = this.parent.y
+  update() {
+    if (this.box.shape === 'rect') {
+      const box = this.box as Rect
+      this.box.x = this.parent.x - box.width / 2
+      this.box.y = this.parent.centerY() - box.height / 2
+    }
+
+    if (this.box.shape === 'arc') {
+      this.box.x = this.parent.x
+      this.box.y = this.parent.y
+    }
+  }
+
+  debug(context: CanvasRenderingContext2D): void {
+    this.box.draw(context)
   }
 }
