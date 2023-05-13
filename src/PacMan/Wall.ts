a75c640724ad05200c94953155ebaae8286018e7
isaque <isaqueclaudino12@gmail.com>
2023-05-13 03:14:56 -0300

    created wall object to game

---
+++ src/PacMan/Wall.ts
@@ -0,0 +1,22 @@
+import BoundingBox from '@Engine/BoundingBox'
+import Rect from '@Engine/Draw/Rect'
+import Object from '@Engine/Object'
+
+export default class Wall extends Object {
+  sprite = new Rect().resize(30, 120).moveTo(250, 150).setColor('#eeeeee')
+  bbox = new BoundingBox(new Rect(), this.sprite)
+
+  constructor(engine) {
+    super(engine)
+    this.bbox.box.resize(this.sprite.width, this.sprite.height)
+  }
+
+  override update = () => {
+    this.bbox.update()
+  }
+
+  override render = () => {
+    this.draw()
+    this.bbox.debug(this.engine.context2d)
+  }
+}
