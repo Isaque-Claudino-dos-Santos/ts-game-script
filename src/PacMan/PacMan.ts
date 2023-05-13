f0ff930368fa2b0c19b34848a1ef6e6717885ac0
isaque <isaqueclaudino12@gmail.com>
2023-05-12 21:30:57 -0300

    testing in pagman game keyboard class

--- src/PacMan/PacMan.ts
+++ src/PacMan/PacMan.ts
@@ -4,7 +4,15 @@ export default class PacMan extends Game {
   override init: TypeGameFN = () => {
     console.log(`Game Init ✅`)
   }
-  override update: TypeGameFN = () => {}
+  override update: TypeGameFN = () => {
+    if (this.engine.keyboard.check('KeyA')) {
+      console.log('On Check 👉 A')
+    }
+
+    if (this.engine.keyboard.check('KeyD')) {
+      console.log('On Check 👉 D')
+    }
+  }
 
   override render: TypeGameFN = () => {}
 }
