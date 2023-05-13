6aea74ad415d6cc7f4eff8e1377c5ecdd62d8928
isaque <isaqueclaudino12@gmail.com>
2023-05-13 02:09:44 -0300

    ajust name

--- src/Interfaces/InterfaceObject.ts
+++ src/Interfaces/InterfaceObject.ts
@@ -1,11 +1,11 @@
-import TypeSprite2d from '@Type/TypeSprite2d'
+import TypeSprite from '@Type/TypeSprite'
 import InterfaceGameLoop from './InterfaceGameLoop'
 import Engine from '@Engine/Engine'
 
 type Types = InterfaceGameLoop
 
 export default interface InterfaceObject extends Types {
-  sprite: TypeSprite2d
+  sprite: TypeSprite
   engine: Engine
 
   draw(): void
