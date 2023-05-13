a1d6d0d35aaa64414689a9c1e43d3a9aae1c7920
isaque <isaqueclaudino12@gmail.com>
2023-05-12 22:32:29 -0300

    created game object interface

---
+++ src/Interfaces/InterfaceGameObject.ts
@@ -0,0 +1,12 @@
+import TypeSprite2d from '@Type/TypeSprite2d'
+import InterfaceGameLoop from './InterfaceGameLoop'
+import Engine from '@Engine/Engine'
+
+type Types = InterfaceGameLoop
+
+export default interface InterfaceGameObject extends Types {
+  sprite: TypeSprite2d
+  engine: Engine
+
+  draw(): void
+}
