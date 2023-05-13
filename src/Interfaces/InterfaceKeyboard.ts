1fdef9eee0f081a1a03863b89b6f880dd89e5a36
isaque <isaqueclaudino12@gmail.com>
2023-05-12 21:30:42 -0300

    created interface keyboard and your declarations

---
+++ src/Interfaces/InterfaceKeyboard.ts
@@ -0,0 +1,15 @@
+import TypeVK from '@Type/TypeVK'
+
+export type TypeKeyboardKey = {
+  isDown: boolean
+  locked: boolean
+}
+export type TypeKeyboardKeys = {
+  [index in TypeVK]: TypeKeyboardKey
+}
+
+export default interface InterfaceKeyboard {
+  readonly keys: TypeKeyboardKeys
+  check(vk: TypeVK): Boolean
+  lockKey(vk: TypeVK): void
+}
