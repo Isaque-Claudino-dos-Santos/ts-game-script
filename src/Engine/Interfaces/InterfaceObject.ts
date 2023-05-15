fd70e0beb9563edffc9279095a03232919874f0c
isaque <isaqueclaudino12@gmail.com>
2023-05-15 13:07:55 -0300

    add readonly

--- src/Engine/Interfaces/InterfaceObject.ts
+++ src/Engine/Interfaces/InterfaceObject.ts
@@ -1,12 +1,14 @@
 import TypeSprite from '@Engine/Types/TypeSprite'
 import InterfaceGameLoop from './InterfaceGameLoop'
 import Engine from '@Engine/Engine'
+import BoundingBox from '@Engine/BoundingBox'
 
 type Types = InterfaceGameLoop
 
 export default interface InterfaceObject extends Types {
-  sprite: TypeSprite
-  engine: Engine
+  readonly sprite: TypeSprite
+  readonly boundingBox: BoundingBox<TypeSprite> | undefined
+  readonly engine: Engine
 
   draw(): void
 }
