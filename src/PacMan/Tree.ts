9641b89c0146fb660781cb720dbbf32735d3518a
isaque <isaqueclaudino12@gmail.com>
2023-05-15 01:51:02 -0300

    add bounding box in tree

--- src/PacMan/Tree.ts
+++ src/PacMan/Tree.ts
@@ -5,9 +5,11 @@ import Rect from '@Engine/Draw/Rect'
 import Object from '@Engine/Modules/Object'
 
 export default class Tree extends Object {
+  public sprite = new MixDraw().moveTo(200, 70)
   private stem = new Rect().resize(20, 70).setColor('#aa4400')
   private sheets = new Rect().resize(30, 30).setColor('#005500').fixTo(-5, -30)
-  public sprite = new MixDraw().moveTo(200, 70)
+  private bboxStem = new BoundingBox(this.stem)
+  private bboxSheets = new BoundingBox(this.sheets)
 
   constructor(engine) {
     super(engine)
@@ -19,9 +21,13 @@ export default class Tree extends Object {
 
   update = () => {
     this.sprite.angle += 0.01
+    this.bboxSheets.update()
+    this.bboxStem.update()
   }
 
   render = () => {
+    this.bboxStem.debug(this.engine.canvas.context)
+    this.bboxSheets.debug(this.engine.canvas.context)
     this.sprite.draw(this.engine.canvas.context)
   }
 }
