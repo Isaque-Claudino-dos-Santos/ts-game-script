1123bff6536cd7b2db63fe9d200bbe193b2c9cdf
isaque <isaqueclaudino12@gmail.com>
2023-05-15 11:14:47 -0300

    declared method onCollider

--- src/Engine/Interfaces/InterfaceBoundingBox.ts
+++ src/Engine/Interfaces/InterfaceBoundingBox.ts
@@ -1,9 +1,13 @@
+import BoundingBox from '@Engine/BoundingBox'
 import TypeSprite from '@Type/TypeSprite'
 
 export default interface InterfaceBoundingBox {
   readonly box: TypeSprite
   sprite: TypeSprite
 
+  onCollision<Collided extends BoundingBox<TypeSprite>>(
+    collided: Collided
+  ): void
   update(): void
   debug(context: CanvasRenderingContext2D): void
 }
