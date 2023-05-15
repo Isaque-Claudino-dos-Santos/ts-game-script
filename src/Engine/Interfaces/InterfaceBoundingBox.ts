5a33b5e7250c366cbd9ee492762f6a7c79ddcc2d
isaque <isaqueclaudino12@gmail.com>
2023-05-15 17:52:19 -0300

    add attribute collided side

--- src/Engine/Interfaces/InterfaceBoundingBox.ts
+++ src/Engine/Interfaces/InterfaceBoundingBox.ts
@@ -1,9 +1,11 @@
 import Object from '@Engine/Modules/Object'
+import TypeSide from '@Engine/Types/TypeSide'
 import TypeSprite from '@Type/TypeSprite'
 
 export default interface InterfaceBoundingBox {
   readonly box: TypeSprite
   readonly object: Object<TypeSprite>
+  readonly collidedSide: TypeSide<boolean>
 
   update(): void
   debug(context: CanvasRenderingContext2D): void
