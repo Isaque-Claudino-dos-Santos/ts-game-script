56deaadc4be3b7ab114010be71ee53b7db24cdb5
isaque <isaqueclaudino12@gmail.com>
2023-05-15 22:01:42 -0300

    created interface collided

---
+++ src/Engine/Interfaces/InterfaceCollided.ts
@@ -0,0 +1,11 @@
+export default interface InterfaceCollided {
+  onLeft: boolean
+  onRight: boolean
+  onTop: boolean
+  onBottom: boolean
+
+  pointLeft: number
+  pointRight: number
+  pointTop: number
+  pointBottom: number
+}
