8365751d696adfb3d6d92b7f5b76c0fcfb4be519
isaque <isaqueclaudino12@gmail.com>
2023-05-16 16:01:11 -0300

    add type sprite

--- src/Engine/Types/TypeSprite.ts
+++ src/Engine/Types/TypeSprite.ts
@@ -1,8 +1,9 @@
 import Arc from '@Engine/Draw/Arc'
 import MixDraw from '@Engine/Draw/MixDraw'
 import Rect from '@Engine/Draw/Rect'
+import Sprite from '@Engine/Draw/Sprite'
 import Text from '@Engine/Draw/Text'
 
-type TypeSprite = Rect | Arc | Text | MixDraw
+type TypeSprite = Rect | Arc | Text | MixDraw | Sprite
 
 export default TypeSprite
