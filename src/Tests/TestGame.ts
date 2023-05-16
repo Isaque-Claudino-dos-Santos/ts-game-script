bc56066aab2011ed62d999252741bff13525cb30
isaque <isaqueclaudino12@gmail.com>
2023-05-16 16:01:30 -0300

    add player in game

--- src/Tests/TestGame.ts
+++ src/Tests/TestGame.ts
@@ -1,3 +1,14 @@
 import Game from '@Engine/Modules/Game'
+import Player from './Player'
 
-export default class TestGame extends Game {}
+export default class TestGame extends Game {
+  readonly player = new Player(this.engine)
+
+  init = () => {
+    this.player.init()
+  }
+
+  render = () => {
+    this.player.render()
+  }
+}
