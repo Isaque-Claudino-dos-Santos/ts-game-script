5a37a1e381908e59d2fe7ffd66d931a92f869655
isaque <isaqueclaudino12@gmail.com>
2023-05-17 01:04:54 -0300

    resize canvas

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import TestGame from './Tests/TestGame'
 
 const engine = new Engine()
 
-engine.canvas.screenSize(550, 350)
-engine.canvas.resolution(550 / 2, 350 / 2)
+engine.canvas.screenSize(760, 350)
+engine.canvas.resolution(760 / 2, 350 / 2)
 
 engine.init(new TestGame(engine))
