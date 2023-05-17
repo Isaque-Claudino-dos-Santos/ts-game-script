d9e90e9c3271fac38b06ea29ebb9eab820315221
isaque <isaqueclaudino12@gmail.com>
2023-05-17 15:41:50 -0300

    redifined resolution

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import TestGame from './Tests/TestGame'
 const engine = new Engine()
 
 engine.canvas.screenSize(760, 350)
-engine.canvas.resolution(760 / 2, 350 / 2)
+engine.canvas.resolution(760 / 3, 350 / 3)
 
 engine.init(new TestGame(engine))
