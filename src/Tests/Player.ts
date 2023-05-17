d4cec1ed0c6ddc83567318a214e3bebdf0f6424f
isaque <isaqueclaudino12@gmail.com>
2023-05-16 23:43:55 -0300

    fixed error boundbox param

--- src/Tests/Player.ts
+++ src/Tests/Player.ts
@@ -2,20 +2,25 @@ import BoundingBox from '@Engine/BoundingBox'
 import Sprite from '@Engine/Draw/Sprite'
 import Object from '@Engine/Modules/Object'
 import img from '@Asset/MotoBoy.png'
+import Rect from '@Engine/Draw/Rect'
 
 export default class Player extends Object<Sprite> {
   readonly sprite = new Sprite()
     .resizeSource(32, 32)
     .moveTo(100, 100)
     .resize(32, 32)
-  readonly boundingBox = new BoundingBox(this)
+  readonly boundingBox: BoundingBox<Rect> = new BoundingBox(this, new Rect())
 
   init = () => {
     this.sprite.setSourceImage(img)
-    console.log(this.sprite)
+  }
+
+  update = () => {
+    this.boundingBox.update()
   }
 
   render = () => {
     this.sprite.draw(this.engine.canvas.context)
+    this.boundingBox.debug(this.engine.canvas.context)
   }
 }
