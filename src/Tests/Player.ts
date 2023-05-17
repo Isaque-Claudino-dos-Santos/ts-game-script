50a6c8a8f4cc857f7b3b599a54486a3a633a73d9
isaque <isaqueclaudino12@gmail.com>
2023-05-17 15:52:19 -0300

    add movimentation

--- src/Tests/Player.ts
+++ src/Tests/Player.ts
@@ -7,11 +7,24 @@ import Rect from '@Engine/Draw/Rect'
 export default class Player extends Object<Sprite> {
   readonly sprite = new Sprite()
   readonly boundingBox: BoundingBox<Rect> = new BoundingBox(this, new Rect())
+  readonly speed = { x: 1, y: 1 }
+
+  movimentation() {
+    const key = this.engine.keyboard
+
+    if (key.check('KeyD')) {
+      this.sprite.addX(this.speed.x)
+    }
+
+    if (key.check('KeyA')) {
+      this.sprite.addX(-this.speed.x)
+    }
+  }
 
   init = () => {
     this.sprite
       .resizeSource(32, 32)
-      .moveTo(10, 10)
+      .moveTo(10, 70)
       .resize(32, 32)
       .setSourceImage(img)
     this.boundingBox.box.resize(15, 32)
@@ -19,6 +32,7 @@ export default class Player extends Object<Sprite> {
   }
 
   update = () => {
+    this.movimentation()
     this.boundingBox.update()
   }
 
