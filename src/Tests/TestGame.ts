5b69b79ed99cac0aa6b6e8d675676d746ba93eef
isaque <isaqueclaudino12@gmail.com>
2023-05-17 15:40:36 -0300

    implemented background in game

--- src/Tests/TestGame.ts
+++ src/Tests/TestGame.ts
@@ -1,10 +1,13 @@
 import Game from '@Engine/Modules/Game'
 import Player from './Player'
+import Background from './Background'
 
 export default class TestGame extends Game {
+  readonly background = new Background(this.engine)
   readonly player = new Player(this.engine)
 
   init = () => {
+    this.background.init()
     this.player.init()
   }
 
@@ -13,6 +16,7 @@ export default class TestGame extends Game {
   }
 
   render = () => {
+    this.background.render()
     this.player.render()
   }
 }
