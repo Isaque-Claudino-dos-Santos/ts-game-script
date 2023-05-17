c659e17a55635e8ad224fc2347ce451f03463d1f
isaque <isaqueclaudino12@gmail.com>
2023-05-16 23:57:33 -0300

    fixed error add execption in methods

--- src/Engine/Collider.ts
+++ src/Engine/Collider.ts
@@ -3,36 +3,54 @@ import Rect from './Draw/Rect'
 import Arc from './Draw/Arc'
 import Object from './Modules/Object'
 import { TypeResolveCollider } from '@Engine/Interfaces/InterfaceCollider'
+import BoundingBox from './BoundingBox'
 
 export default class Collider implements InterfaceCollider {
+  private exceptionTypeBoundBox(msgType: string): never {
+    throw console.log(
+      new Error(
+        `The bounding box of object not is type BoundingBox<${msgType}>`
+      )
+    )
+  }
+
   rectWithRect(
     object1: Object<Rect>,
     object2: Object<Rect>,
     resolve: TypeResolveCollider<typeof object2>
-  ): this {
-    const box1 = object1.boundingBox
-    const box2 = object2.boundingBox
+  ): this | never {
+    const boundingBox1 = object1.boundingBox as BoundingBox<Rect>
+    const boundingBox2 = object2.boundingBox as BoundingBox<Rect>
+    const box1 = boundingBox1.box
+    const box2 = boundingBox1.box
+
+    if (
+      !(boundingBox1 instanceof BoundingBox<Rect>) ||
+      !(boundingBox2 instanceof BoundingBox<Rect>)
+    ) {
+      this.exceptionTypeBoundBox('Rect')
+    }
 
     if (!box1 || !box2) return this
 
-    const collidedLeft = box1.box.x + box1.box.width <= box2.box.x
-    const collidedRight = box1.box.x <= box2.box.x + box2.box.width
-    const colliderTop = box1.box.y + box1.box.height >= box2.box.y
-    const collidedBottom = box1.box.y <= box2.box.y + box2.box.height
+    const collidedLeft = box1.x + box1.width <= box2.x
+    const collidedRight = box1.x <= box2.x + box2.width
+    const colliderTop = box1.y + box1.height >= box2.y
+    const collidedBottom = box1.y <= box2.y + box2.height
     const collidedAny =
       collidedRight && collidedLeft && colliderTop && collidedBottom
 
-    box2.collided.distanceLeft = box1.box.x + box1.box.width - box2.box.x
-    box2.collided.distanceRight = box1.box.x - box2.box.x + box2.box.width
-    box2.collided.distanceTop = box1.box.y + box1.box.height - box2.box.y
-    box2.collided.distanceBottom = box1.box.y - box2.box.y + box2.box.height
+    boundingBox2.collided.distanceLeft = box1.x + box1.width - box2.x
+    boundingBox2.collided.distanceRight = box1.x - box2.x + box2.width
+    boundingBox2.collided.distanceTop = box1.y + box1.height - box2.y
+    boundingBox2.collided.distanceBottom = box1.y - box2.y + box2.height
 
     if (collidedAny) {
-      box2.collided.onLeft = collidedLeft
-      box2.collided.onRight = collidedRight
-      box2.collided.onTop = colliderTop
-      box2.collided.onBottom = collidedBottom
-      box2.collided.onAny = collidedAny
+      boundingBox2.collided.onLeft = collidedLeft
+      boundingBox2.collided.onRight = collidedRight
+      boundingBox2.collided.onTop = colliderTop
+      boundingBox2.collided.onBottom = collidedBottom
+      boundingBox2.collided.onAny = collidedAny
 
       resolve(object2)
     }
@@ -44,41 +62,47 @@ export default class Collider implements InterfaceCollider {
     object2: Object<Arc>,
     resolve: TypeResolveCollider<typeof object2>
   ): this {
-    const box1 = object1.boundingBox
-    const box2 = object2.boundingBox
+    const boundingBox1 = object1.boundingBox as BoundingBox<Arc>
+    const boundingBox2 = object2.boundingBox as BoundingBox<Arc>
+    const box1 = boundingBox1.box
+    const box2 = boundingBox2.box
+    if (
+      !(boundingBox1 instanceof BoundingBox<Rect>) ||
+      !(boundingBox2 instanceof BoundingBox<Rect>)
+    ) {
+      this.exceptionTypeBoundBox('Rect')
+    }
 
-    if (!box1 || !box2) return this
-
-    const catX = box1.box.x - box2.box.x
-    const catY = box1.box.y - box2.box.y
-    const sumRadius = box1.box.radius + box2.box.radius
+    const catX = box1.x - box2.x
+    const catY = box1.y - box2.y
+    const sumRadius = box1.radius + box2.radius
     const hpty = Math.sqrt(catX ** 2 + catY ** 2)
     const overlap = hpty - sumRadius
 
-    const isOnLeft = box1.box.x < box2.box.x
-    const isOnRight = box1.box.x > box2.box.x
-    const isOnTop = box1.box.y < box2.box.y
-    const isOnBottom = box1.box.y > box2.box.y
+    const isOnLeft = box1.x < box2.x
+    const isOnRight = box1.x > box2.x
+    const isOnTop = box1.y < box2.y
+    const isOnBottom = box1.y > box2.y
 
     const collidedLeft = isOnLeft && overlap <= 0
     const collidedRight = isOnRight && overlap <= 0
     const colliderTop = isOnTop && overlap <= 0
     const collidedBottom = isOnBottom && overlap <= 0
 
-    box2.collided.onLeft = collidedLeft
-    box2.collided.onRight = collidedRight
-    box2.collided.onTop = colliderTop
-    box2.collided.onBottom = collidedBottom
-    box2.collided.onAny = overlap <= 0
+    boundingBox2.collided.onLeft = collidedLeft
+    boundingBox2.collided.onRight = collidedRight
+    boundingBox2.collided.onTop = colliderTop
+    boundingBox2.collided.onBottom = collidedBottom
+    boundingBox2.collided.onAny = overlap <= 0
 
-    box2.collided.distanceLeft =
-      box1.box.x + box1.box.radius - box2.box.x + box2.box.radius
-    box2.collided.distanceRight =
-      box1.box.x + box1.box.radius - box2.box.x + box2.box.radius
-    box2.collided.distanceTop =
-      box1.box.y + box1.box.radius - box2.box.y + box2.box.radius
-    box2.collided.distanceBottom =
-      box1.box.y + box1.box.radius - box2.box.y + box2.box.radius
+    boundingBox2.collided.distanceLeft =
+      box1.x + box1.radius - box2.x + box2.radius
+    boundingBox2.collided.distanceRight =
+      box1.x + box1.radius - box2.x + box2.radius
+    boundingBox2.collided.distanceTop =
+      box1.y + box1.radius - box2.y + box2.radius
+    boundingBox2.collided.distanceBottom =
+      box1.y + box1.radius - box2.y + box2.radius
 
     if (overlap <= 0) {
       resolve(object2)
@@ -91,46 +115,54 @@ export default class Collider implements InterfaceCollider {
     object2: Object<Rect>,
     resolve: TypeResolveCollider<typeof object2>
   ): this {
-    const box1 = object1.boundingBox
-    const box2 = object2.boundingBox
+    const boundingBox1 = object1.boundingBox as BoundingBox<Arc>
+    const boundingBox2 = object2.boundingBox as BoundingBox<Rect>
 
-    if (!box1 || !box2) return this
+    const box1 = boundingBox1.box
+    const box2 = boundingBox2.box
 
-    let pointX = box1.box.x
-    let pointY = box1.box.y
+    if (
+      !(boundingBox1 instanceof BoundingBox<Rect>) ||
+      !(boundingBox2 instanceof BoundingBox<Rect>)
+    ) {
+      this.exceptionTypeBoundBox('Rect')
+    }
 
-    const isOnLeft = box1.box.x < box2.box.x
-    const isOnRight = box1.box.x > box2.box.x + box2.box.width
-    const isOnTop = box1.box.y < box2.box.y
-    const isOnBottom = box1.box.y > box2.box.y + box2.box.height
+    let pointX = box1.x
+    let pointY = box1.y
 
-    if (isOnLeft) pointX = box2.box.x
-    if (isOnRight) pointX = box2.box.x + box2.box.width
-    if (isOnTop) pointY = box2.box.y
-    if (isOnBottom) pointY = box2.box.y + box2.box.height
+    const isOnLeft = box1.x < box2.x
+    const isOnRight = box1.x > box2.x + box2.width
+    const isOnTop = box1.y < box2.y
+    const isOnBottom = box1.y > box2.y + box2.height
 
-    const catX = box1.box.x - pointX
-    const catY = box1.box.y - pointY
+    if (isOnLeft) pointX = box2.x
+    if (isOnRight) pointX = box2.x + box2.width
+    if (isOnTop) pointY = box2.y
+    if (isOnBottom) pointY = box2.y + box2.height
+
+    const catX = box1.x - pointX
+    const catY = box1.y - pointY
     const hpty = Math.sqrt(catX ** 2 + catY ** 2)
-    const overlap = hpty - box1.box.radius
+    const overlap = hpty - box1.radius
 
     const collidedLeft = isOnLeft && overlap <= 0
     const collidedRight = isOnRight && overlap <= 0
     const colliderTop = isOnTop && overlap <= 0
     const collidedBottom = isOnBottom && overlap <= 0
 
-    box2.collided.onLeft = collidedLeft
-    box2.collided.onRight = collidedRight
-    box2.collided.onTop = colliderTop
-    box2.collided.onBottom = collidedBottom
-    box2.collided.onAny = overlap <= 0
+    boundingBox2.collided.onLeft = collidedLeft
+    boundingBox2.collided.onRight = collidedRight
+    boundingBox2.collided.onTop = colliderTop
+    boundingBox2.collided.onBottom = collidedBottom
+    boundingBox2.collided.onAny = overlap <= 0
 
-    box2.collided.distanceLeft = box1.box.x + box1.box.radius - box2.box.x
-    box2.collided.distanceRight =
-      box1.box.x + box1.box.radius - box2.box.x + box2.box.width
-    box2.collided.distanceTop = box1.box.y + box1.box.radius - box2.box.y
-    box2.collided.distanceBottom =
-      box1.box.y + box1.box.radius - box2.box.y + box2.box.height
+    boundingBox2.collided.distanceLeft = box1.x + box1.radius - box2.x
+    boundingBox2.collided.distanceRight =
+      box1.x + box1.radius - box2.x + box2.width
+    boundingBox2.collided.distanceTop = box1.y + box1.radius - box2.y
+    boundingBox2.collided.distanceBottom =
+      box1.y + box1.radius - box2.y + box2.height
 
     if (overlap <= 0) {
       resolve(object2)
