05979f46cb8ca935808dd3fe9fe79e26a24d9c45
isaque <isaqueclaudino12@gmail.com>
2023-05-17 17:10:04 -0300

    implemented object pistol

--- src/Tests/Player.ts
+++ src/Tests/Player.ts
@@ -3,11 +3,14 @@ import Sprite from '@Engine/Draw/Sprite'
 import Object from '@Engine/Modules/Object'
 import img from '@Asset/MotoBoy.png'
 import Rect from '@Engine/Draw/Rect'
+import Pistol from './Pistol'
 
 export default class Player extends Object<Sprite> {
   readonly sprite = new Sprite()
   readonly boundingBox: BoundingBox<Rect> = new BoundingBox(this, new Rect())
   readonly speed = { x: 1, y: 1 }
+
+  readonly gun = new Pistol(this.engine)
 
   movimentation() {
     const key = this.engine.keyboard
@@ -22,6 +25,7 @@ export default class Player extends Object<Sprite> {
   }
 
   init = () => {
+    this.gun.init()
     this.sprite
       .resizeSource(32, 32)
       .moveTo(10, 70)
@@ -33,10 +37,16 @@ export default class Player extends Object<Sprite> {
 
   update = () => {
     this.movimentation()
+    this.gun.sprite.moveTo(
+      this.sprite.getOriginX() + 20,
+      this.sprite.getOriginY() + 12
+    )
+    this.gun.update()
     this.boundingBox.update()
   }
 
   render = () => {
+    this.gun.draw()
     this.sprite.draw(this.engine.canvas.context)
   }
 }
