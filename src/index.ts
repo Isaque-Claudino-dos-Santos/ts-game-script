d6d09ed298febd96050428ee7a0437e841bf2777
isaque <isaqueclaudino12@gmail.com>
2023-05-17 00:39:03 -0300

    add resolution

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ import TestGame from './Tests/TestGame'
 const engine = new Engine()
 
 engine.canvas.screenSize(550, 350)
+engine.canvas.resolution(550 / 2, 350 / 2)
 
 engine.init(new TestGame(engine))
