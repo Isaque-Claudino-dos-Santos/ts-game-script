e13329b6f7f8a5c3e5e6702c9381ddc104d492f4
isaque <isaqueclaudino12@gmail.com>
2023-05-17 22:43:34 -0300

    boting sceneSky

--- src/FlappyBird/FlappyBird.ts
+++ src/FlappyBird/FlappyBird.ts
@@ -1,3 +1,10 @@
 import Game from '@Engine/Modules/Game'
+import SceneSky from './Scenes/SceneSky'
 
-export default class FlappyBird extends Game {}
+export default class FlappyBird extends Game {
+  sceneSky = new SceneSky(this.engine)
+
+  init = () => this.sceneSky.init()
+  update = () => this.sceneSky.update()
+  render = () => this.sceneSky.render()
+}
