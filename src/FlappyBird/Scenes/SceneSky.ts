04144989eef4563e2da1a4cd3fd14e8743883c6c
isaque <isaqueclaudino12@gmail.com>
2023-05-17 22:46:47 -0300

    add object bird

--- src/FlappyBird/Scenes/SceneSky.ts
+++ src/FlappyBird/Scenes/SceneSky.ts
@@ -1,7 +1,8 @@
 import Object from '@Engine/Modules/Object'
 import Scene from '@Engine/Modules/Scene'
 import TypeSprite from '@Engine/Types/TypeSprite'
+import Bird from '../Objects/Bird'
 
 export default class SceneSky extends Scene {
-  readonly objects: Object<TypeSprite>[] = []
+  readonly objects: Object<TypeSprite>[] = [new Bird(this.engine)]
 }
