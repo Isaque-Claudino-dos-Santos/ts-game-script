0b654288d774291e945b2c88bfe91400d8b7b009
isaque <isaqueclaudino12@gmail.com>
2023-05-17 23:23:24 -0300

    configured sprite bird

--- src/FlappyBird/Objects/Bird.ts
+++ src/FlappyBird/Objects/Bird.ts
@@ -3,11 +3,20 @@ import Rect from '@Engine/Draw/Rect'
 import Sprite from '@Engine/Draw/Sprite'
 import Object from '@Engine/Modules/Object'
 
+import imgBird from '@Asset/Bird.png'
+
 export default class Bird extends Object<Sprite> {
   sprite: Sprite = new Sprite()
   boundingBox: BoundingBox<Rect> = new BoundingBox(this, new Rect())
 
-  init = () => {}
+  init = () => {
+    this.sprite
+      .setSourceImage(imgBird)
+      .resize(32, 32)
+      .resizeSource(32, 32)
+      .moveTo(10, 10)
+      .moveSourceTo(32, 0)
+  }
 
   update = () => {
     this.boundingBox.update()
