1eacd879d0baf1f70370d21c145162c19837f715
isaque <isaqueclaudino12@gmail.com>
2023-05-17 23:26:23 -0300

    created background

---
+++ src/FlappyBird/Objects/Background.ts
@@ -0,0 +1,20 @@
+import BoundingBox from '@Engine/BoundingBox'
+import Rect from '@Engine/Draw/Rect'
+import Object from '@Engine/Modules/Object'
+import TypeSprite from '@Engine/Types/TypeSprite'
+
+export default class Background extends Object<Rect> {
+  sprite: Rect = new Rect()
+  boundingBox: BoundingBox<Rect> = new BoundingBox(this, new Rect())
+
+  init = () => {
+    this.sprite
+      .moveTo(0, 0)
+      .resize(this.engine.canvas.width(), this.engine.canvas.height())
+      .setColor('#00ffee')
+  }
+
+  render = () => {
+    this.sprite.draw(this.engine.canvas.context)
+  }
+}
