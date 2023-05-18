68b95e4a1168039b33db95d51c99f72a5c735640
isaque <isaqueclaudino12@gmail.com>
2023-05-17 23:27:52 -0300

    add background

--- src/FlappyBird/Scenes/SceneSky.ts
+++ src/FlappyBird/Scenes/SceneSky.ts
@@ -2,7 +2,11 @@ import Object from '@Engine/Modules/Object'
 import Scene from '@Engine/Modules/Scene'
 import TypeSprite from '@Engine/Types/TypeSprite'
 import Bird from '../Objects/Bird'
+import Background from '../Objects/Background'
 
 export default class SceneSky extends Scene {
-  readonly objects: Object<TypeSprite>[] = [new Bird(this.engine)]
+  readonly objects: Object<TypeSprite>[] = [
+    new Bird(this.engine),
+    new Background(this.engine),
+  ]
 }
