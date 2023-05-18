3e4adc328c737a6f3dfaaf5536430b030558217c
isaque <isaqueclaudino12@gmail.com>
2023-05-17 22:40:57 -0300

    created scene

---
+++ src/FlappyBird/Scenes/SceneSky.ts
@@ -0,0 +1,7 @@
+import Object from '@Engine/Modules/Object'
+import Scene from '@Engine/Modules/Scene'
+import TypeSprite from '@Engine/Types/TypeSprite'
+
+export default class SceneSky extends Scene {
+  readonly objects: Object<TypeSprite>[] = []
+}
