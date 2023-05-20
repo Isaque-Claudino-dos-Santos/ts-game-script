f1a51725d4906db78796204c97b5701d7f5eadb5
isaque <isaqueclaudino12@gmail.com>
2023-05-19 21:15:56 -0300

    created element to view screen size and resolution

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,9 @@ engine.canvas.screenSize(640, 384)
 engine.canvas.resolution(640 * 6, 384 * 6)
 
 engine.init(new FlappyBird(engine))
+
+const screenSizeElement = document.querySelector('.screenSize') as HTMLElement
+const resolutionElement = document.querySelector('.resolution') as HTMLElement
+
+screenSizeElement.textContent = `${engine.canvas.screenWidth} x ${engine.canvas.screenHeight}`
+resolutionElement.textContent = `${engine.canvas.width()} x ${engine.canvas.height()}`
