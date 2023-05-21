44577d6595c2c1c01f84224a31b953d5c27ce2f5
isaque <isaqueclaudino12@gmail.com>
2023-05-20 22:27:16 -0300

    add pipe in screen

--- src/FlappyBird/Scenes/SceneSky.ts
+++ src/FlappyBird/Scenes/SceneSky.ts
@@ -3,10 +3,12 @@ import Scene from '@Engine/Modules/Scene'
 import TypeSprite from '@Engine/Types/TypeSprite'
 import Bird from '../Objects/Bird'
 import Background from '../Objects/Background'
+import Pipe from '../Objects/Pipe'
 
 export default class SceneSky extends Scene {
   readonly objects: Object<TypeSprite>[] = [
     new Background(this.engine),
     new Bird(this.engine),
+    new Pipe(this.engine),
   ]
 }
