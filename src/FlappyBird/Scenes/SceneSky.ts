50bcb11b42668d83aa953db892838d5e9ff99a0a
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-05-22 17:26:38 -0300

    removed imports

--- src/FlappyBird/Scenes/SceneSky.ts
+++ src/FlappyBird/Scenes/SceneSky.ts
@@ -1,6 +1,4 @@
-import Object from '@Engine/Modules/Object'
 import Scene from '@Engine/Modules/Scene'
-import TypeSprite from '@Engine/Types/TypeSprite'
 import Bird from '../Objects/Bird'
 import Background from '../Objects/Background'
 import Pipe from '../Objects/Pipe'
