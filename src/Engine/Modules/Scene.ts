b1a4d4414f6deb0cc3c69b9edc7d3e627f9d4886
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-05-22 17:16:16 -0300

    add method to forEach by objects and add method collied

--- src/Engine/Modules/Scene.ts
+++ src/Engine/Modules/Scene.ts
@@ -1,24 +1,44 @@
-import InterfaceScene from '@Engine/Interfaces/InterfaceScene'
+import InterfaceScene, {
+  TypeSceneObjects,
+} from '@Engine/Interfaces/InterfaceScene'
 import TypeSprite from '@Engine/Types/TypeSprite'
 import Object from './Object'
 import Engine from '@Engine/Engine'
 
 export default abstract class Scene implements InterfaceScene {
-  abstract objects: Object<TypeSprite>[]
+  abstract objects: TypeSceneObjects
   enable: boolean = true
 
   constructor(public readonly engine: Engine) {}
 
+  objectsForEach(
+    callback: (
+      object: Object<TypeSprite>,
+      name: string,
+      objects: TypeSceneObjects
+    ) => void
+  ): void {
+    for (const name in this.objects) {
+      const object = this.objects[name]
+      callback(object, name, this.objects)
+    }
+  }
+
   readonly init = () => {
     if (!this.enable) return
-    this.objects.forEach((obj) => obj.init())
+    this.objectsForEach((obj) => obj.init())
   }
   readonly update = () => {
     if (!this.enable) return
-    this.objects.forEach((obj) => obj.update())
+    this.objectsForEach((obj) => obj.update())
   }
   readonly render = () => {
     if (!this.enable) return
-    this.objects.forEach((obj) => obj.render())
+    this.objectsForEach((obj) => obj.render())
+  }
+
+  readonly collided = () => {
+    if (!this.enable) return
+    this.objectsForEach((obj) => obj.collided())
   }
 }
