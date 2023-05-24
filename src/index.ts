1e2325f4e2cde024caf107219212a50f2db370f5
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-05-23 22:32:21 -0300

    merge new feature of game bird

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import Engine from '@Engine/Engine'
-import FlappyBird from './FlappyBird/FlappyBird'
 
 const engine = new Engine()
 
 engine.canvas.screenSize(640, 384)
 engine.canvas.resolution(640 * 6, 384 * 6)
 
-engine.init(new FlappyBird(engine))
+// Add Game Instance With param
+// engine.init()
 
 const screenSizeElement = document.querySelector('.screenSize') as HTMLElement
 const resolutionElement = document.querySelector('.resolution') as HTMLElement
