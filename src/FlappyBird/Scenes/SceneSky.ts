bc4077109d879a925b8ac7fc86fbe951414cb26a
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-05-23 22:12:49 -0300

    removed obstacle

--- src/FlappyBird/Scenes/SceneSky.ts
+++ src/FlappyBird/Scenes/SceneSky.ts
@@ -1,12 +1,10 @@
 import Scene from '@Engine/Modules/Scene'
 import Bird from '../Objects/Bird'
 import Background from '../Objects/Background'
-import Obstacle from '../Objects/Obstacle'
 
 export default class SceneSky extends Scene {
   readonly objects = {
     background: new Background(this.engine),
     bird: new Bird(this.engine),
-    obstracle1: new Obstacle(this.engine),
   }
 }
