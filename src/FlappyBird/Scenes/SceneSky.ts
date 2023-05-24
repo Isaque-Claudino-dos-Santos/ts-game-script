a76c24b2ee1e07e1fae18c3bd306924b750fe9c1
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-05-23 21:31:47 -0300

    add first obstacle to testing

--- src/FlappyBird/Scenes/SceneSky.ts
+++ src/FlappyBird/Scenes/SceneSky.ts
@@ -1,19 +1,12 @@
 import Scene from '@Engine/Modules/Scene'
 import Bird from '../Objects/Bird'
 import Background from '../Objects/Background'
-import Pipe from '../Objects/Pipe'
+import Obstacle from '../Objects/Obstacle'
 
 export default class SceneSky extends Scene {
   readonly objects = {
     background: new Background(this.engine),
     bird: new Bird(this.engine),
-    pipe: new Pipe(this.engine),
-  }
-
-  collided = () => {
-    const { bird, pipe } = this.objects
-    this.engine.collider.rectWithRect(bird, pipe, () => {
-      bird.dead()
-    })
+    obstracle1: new Obstacle(this.engine),
   }
 }
