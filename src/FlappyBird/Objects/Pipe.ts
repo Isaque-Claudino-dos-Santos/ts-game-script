aac3e04285ba9ebdd0eb465300ffd511cdf046ec
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-05-23 21:31:26 -0300

    fix error boundbox

--- src/FlappyBird/Objects/Pipe.ts
+++ src/FlappyBird/Objects/Pipe.ts
@@ -6,13 +6,8 @@ export default class Pipe extends Object<Rect> {
   sprite: Rect = new Rect()
   boundingBox: BoundingBox<Rect> = new BoundingBox(this, new Rect())
 
-  init = () => {
-    this.sprite.resize(300, 1000).moveTo(900, 0).setColor('#000')
-    this.boundingBox.box.resize(this.sprite.width, this.sprite.height)
-  }
-
   update = () => {
-    this.sprite.addX(-2)
+    this.boundingBox.box.resize(this.sprite.width, this.sprite.height)
     this.boundingBox.update()
   }
 
