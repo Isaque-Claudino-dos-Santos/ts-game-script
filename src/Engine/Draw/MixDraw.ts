46358380af0d359f64aae8e3ea9fe129ea167a81
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-05-23 21:30:30 -0300

    refectory to use object

--- src/Engine/Draw/MixDraw.ts
+++ src/Engine/Draw/MixDraw.ts
@@ -1,28 +1,34 @@
 import InterfaceMixDraw from '@Engine/Interfaces/InterfaceMixDraw'
 import { TypeShape } from '@Engine/Interfaces/InterfaceShape'
+import Object from '@Engine/Modules/Object'
 import Shape from '@Engine/Modules/Shape'
 import TypeSprite from '@Engine/Types/TypeSprite'
 
 export default class MixDraw extends Shape implements InterfaceMixDraw {
   readonly shape: TypeShape = 'mix'
-  shapes: TypeSprite[] = []
+  objects: Object<TypeSprite>[] = []
 
   copy<MixDraw>(): MixDraw {
-    return Object.assign(new MixDraw(), this) as MixDraw
+    return window.Object.assign(new MixDraw(), this) as MixDraw
   }
 
-  add(shape: TypeSprite): void {
-    this.shapes.push(shape)
+  add(object: Object<TypeSprite>): void {
+    this.objects.push(object)
+  }
+
+  callUpdates(): void {
+    this.objects.forEach((o) => o.update())
   }
 
   draw(context: CanvasRenderingContext2D): this {
     if (!this.enable) return this
-    this.shapes.forEach((shape) => {
-      shape.x = this.getOriginX() + shape.fixedX
-      shape.y = this.getOriginY() + shape.fixedY
-      shape.angle = this.angle
-      shape.draw(context)
+    this.objects.forEach(({ sprite }) => {
+      sprite.x = this.getOriginX() + sprite.fixedX
+      sprite.y = this.getOriginY() + sprite.fixedY
+      sprite.angle = this.angle
+      sprite.draw(context)
     })
+    this.objects.forEach((o) => o.render())
     return this
   }
 }
