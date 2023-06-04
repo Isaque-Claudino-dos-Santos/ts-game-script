11a4dd30139925363dc39954ff40b3248d4cc954
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-06-04 20:57:46 -0300

    removed

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import GameFlapyBird from './GameFlapyBird'
 import engine from './engine'
 
-engine.init(new GameFlapyBird(engine))
-
-const screenSizeElement = document.querySelector('.screenSize') as HTMLElement
-const resolutionElement = document.querySelector('.resolution') as HTMLElement
-
-screenSizeElement.textContent = `${engine.canvas.screenWidth} x ${engine.canvas.screenHeight}`
-resolutionElement.textContent = `${engine.canvas.width()} x ${engine.canvas.height()}`
+// engine.init()
