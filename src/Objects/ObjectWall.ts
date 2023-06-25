41660ca494493de81295289e49a066e414f2be3a
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-06-25 18:27:15 -0300

    removed index

--- src/Objects/ObjectWall.ts
+++ src/Objects/ObjectWall.ts
@@ -5,7 +5,7 @@ export default class ObjectWall extends Object<Rect> {
   sprite: Rect = new Rect()
 
   init = () => {
-    this.sprite.moveTo(100, 50).resize(30, 100).setColor('#552222').setIndex(3)
+    this.sprite.moveTo(100, 50).resize(30, 100).setColor('#552222')
   }
 
   update = () => {}
