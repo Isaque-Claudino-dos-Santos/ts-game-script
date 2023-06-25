623b43abe56520f4c0834a42b8b677d65880ed06
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-06-25 18:30:50 -0300

    feat: move method  to contructor

--- vendor/Engine/Modules/Render.ts
+++ vendor/Engine/Modules/Render.ts
@@ -5,14 +5,15 @@ import TypeShape from '@Vendor/Types/TypeShape'
 export default class Render implements InterfaceRender {
   shapes: TypeShape[] = []
 
-  constructor(readonly engine: Engine) {}
+  constructor(readonly engine: Engine) {
+    this.ajustIndex()
+  }
 
   private ajustIndex() {
     this.shapes = this.shapes.sort((a, b) => a.index - b.index)
   }
 
   all(): void {
-    this.ajustIndex()
     this.shapes.forEach((o) => o.draw(this.engine.canvas.context))
   }
 
