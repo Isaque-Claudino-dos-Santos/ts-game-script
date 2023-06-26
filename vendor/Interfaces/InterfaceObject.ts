32b7604c0b81d19f7c870449ad47ce1441c3d04e
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-06-26 18:14:25 -0300

    feat: removed scene attribute

--- vendor/Interfaces/InterfaceObject.ts
+++ vendor/Interfaces/InterfaceObject.ts
@@ -1,11 +1,9 @@
 import TypeShape from '@Vendor/Types/TypeShape'
 import InterfaceLoop from './InterfaceLoop'
 import Engine from '@Vendor/Engine'
-import Scene from '@Vendor/Models/Scene'
 
 export default interface InterfaceObject extends InterfaceLoop {
   readonly sprite: TypeShape
   readonly engine: Engine
-  readonly scene: Scene | null
   draw(): void
 }
--- vendor/Models/Object.ts
+++ vendor/Models/Object.ts
@@ -1,14 +1,13 @@
 import Engine from '@Vendor/Engine'
 import InterfaceObject from '@Vendor/Interfaces/InterfaceObject'
 import TypeShape from '@Vendor/Types/TypeShape'
-import Scene from './Scene'
 
 export default abstract class Object<Sprite extends TypeShape>
   implements InterfaceObject
 {
   abstract readonly sprite: Sprite
 
-  constructor(public engine: Engine, public scene: Scene | null = null) {}
+  constructor(public engine: Engine) {}
 
   draw(): void {
     this.sprite.draw(this.engine.canvas.context)
