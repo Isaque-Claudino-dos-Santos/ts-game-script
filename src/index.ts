a2741af6ebc7b738754fe6587482c6209ed80cf9
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-08-04 02:30:04 -0300

    feat: clear bg and rotate rect

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,28 @@
 import Loop from '@Vendor/Events/Loop'
 import Rect from '@Vendor/Graphics2D/Geometries2D/Rect'
 import Canvas2D from '@Vendor/Graphics2D/Canvas2D'
+import Mouse from '@Vendor/Events/Mouse'
 
 const canvas = new Canvas2D()
+const mouse = new Mouse(canvas.screen)
 const loop = new Loop()
 
 const r1 = new Rect()
 
 loop.init = () => {
-  r1.width(30).height(30).x(10).y(10).bgColor('#343434')
+  r1.width(30)
+    .height(30)
+    .x(10)
+    .y(10)
+    .bgColor('#343434')
+    .originX(r1.halfWidth())
+    .originY(r1.halfHeight())
 }
 
 loop.render = () => {
-  r1.draw(canvas.context, (c) => {})
+  canvas.context.clearRect(0, 0, 1000, 1000)
+  r1.angle(r1.angle() + 1)
+  r1.draw(canvas.context)
 }
 
 loop.boot()
