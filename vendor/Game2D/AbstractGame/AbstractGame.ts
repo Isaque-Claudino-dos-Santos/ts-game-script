d02ddcd99fb77b9449a5a29529dc09e49ce4337f
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-08-12 18:59:04 -0300

    feat: add attributes mouse, keyboard and screen

--- vendor/Game2D/AbstractGame/AbstractGame.ts
+++ vendor/Game2D/AbstractGame/AbstractGame.ts
@@ -1,10 +1,17 @@
+import Keyboard from '@Vendor/Events/Keyboard'
+import Mouse from '@Vendor/Events/Mouse'
 import HandlerScenes from '../HandlerScenes'
 import InterfaceAbstractGame from './InterfaceAbstractGame'
 
 export default abstract class AbstractGame implements InterfaceAbstractGame {
   readonly scenes: HandlerScenes = new HandlerScenes(this)
+  readonly mouse: Mouse = new Mouse(this.screen)
+  readonly keyboard: Keyboard = new Keyboard()
 
-  constructor(readonly context: CanvasRenderingContext2D) {}
+  constructor(
+    readonly screen: HTMLCanvasElement,
+    readonly context: CanvasRenderingContext2D
+  ) {}
 
   gameInit = () => {
     this.scenes.getCurrent().callInitInObjects()
--- vendor/Game2D/AbstractGame/InterfaceAbstractGame.d.ts
+++ vendor/Game2D/AbstractGame/InterfaceAbstractGame.d.ts
@@ -1,8 +1,13 @@
+import Mouse from '@Vendor/Events/Mouse'
 import HandlerScenes from '../HandlerScenes'
+import Keyboard from '@Vendor/Events/Keyboard'
 
 export default interface InterfaceAbstractGame {
   readonly scenes: HandlerScenes
+  readonly screen: HTMLCanvasElement
   readonly context: CanvasRenderingContext2D
+  readonly mouse: Mouse
+  readonly keyboard: Keyboard
 
   gameInit(): void
   gameUpdate(): void
