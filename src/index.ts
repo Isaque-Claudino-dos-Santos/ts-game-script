5ef8bd56615e2cf570b0b3277a3d2315dfd7d09e
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-08-12 18:48:03 -0300

    feat: implement new feature

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,21 @@
 import Loop from '@Vendor/Events/Loop'
-import Rect from '@Vendor/Graphics2D/Geometries2D/Rect'
 import Canvas2D from '@Vendor/Graphics2D/Canvas2D'
 import Mouse from '@Vendor/Events/Mouse'
+import GameMyGame from './GameMyGame'
 
 const canvas = new Canvas2D()
 const mouse = new Mouse(canvas.screen)
 const loop = new Loop()
 
-const r1 = new Rect()
+const game = new GameMyGame(canvas.context)
 
 loop.init = () => {
-  r1.width(30)
-    .height(30)
-    .x(10)
-    .y(10)
-    .bgColor('#343434')
-    .originX(r1.halfWidth())
-    .originY(r1.halfHeight())
+  game.gameInit()
 }
 
 loop.render = () => {
   canvas.context.clearRect(0, 0, 1000, 1000)
-  r1.angle(r1.angle() + 1)
-  r1.draw(canvas.context)
+  game.gameRender()
 }
 
 loop.boot()
