2f52bc276b7b92e62e8817ebd0e5873d75a7ee9c
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-08-12 19:19:10 -0300

    feat: passed code to beforeInit method

--- src/GameMyGame.ts
+++ src/GameMyGame.ts
@@ -2,8 +2,7 @@ import AbstractGame from '@Vendor/Game2D/AbstractGame'
 import SceneMain from './MainScene'
 
 export default class GameMyGame extends AbstractGame {
-  constructor(screen: HTMLCanvasElement, context: CanvasRenderingContext2D) {
-    super(screen, context)
+  override beforeInit(): void {
     this.scenes.add('main', new SceneMain(this)).setCurrent('main')
   }
 }
