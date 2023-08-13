47f9edfcdd9e50fc124bbea6ebc8b1bb57c6ed4c
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-08-13 00:42:59 -0300

    feat: add clear rect

--- src/GameMyGame.ts
+++ src/GameMyGame.ts
@@ -5,4 +5,8 @@ export default class GameMyGame extends AbstractGame {
   override beforeInit(): void {
     this.scenes.add('main', new SceneMain(this)).setCurrent('main')
   }
+
+  override beforeRender(): void {
+    this.canvas.clearScreen()
+  }
 }
