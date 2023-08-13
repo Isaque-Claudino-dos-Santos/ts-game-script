edf8a3307ff6e68e9e9170618c9d3adff03fa1cf
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-08-13 01:09:17 -0300

    feat: add box  in scene

--- src/MainScene.ts
+++ src/MainScene.ts
@@ -1,8 +1,10 @@
 import AbstractScene, { SceneObjects } from '@Vendor/Game2D/AbstractScene'
 import ObjectPlayer from './ObjectPlayer'
+import ObjectBox from './ObjectBox'
 
 export default class SceneMain extends AbstractScene {
   objects: SceneObjects = {
     player: new ObjectPlayer(this.game),
+    box: new ObjectBox(this.game, { x: 100, y: 44 }),
   }
 }
