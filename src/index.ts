10461151e5594582da062e8f49a845144793a125
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-08-13 00:43:09 -0300

    fix: fixed new feature

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import Engine2D from '@Vendor/Engine2D'
 import GameMyGame from './GameMyGame'
 
-const engine = new Engine2D()
-const game = new GameMyGame(engine.canvas)
+const game = new GameMyGame()
+const engine = new Engine2D(game)
 
-engine.initialize(game, () => {
+engine.initialize(() => {
   console.log('Initialized Game!')
 })
