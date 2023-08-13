728d20c3c4187b303a80eb1d51946f437ad7e1e5
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-08-13 02:43:06 -0300

    feat: implemnted boundingbox attribute

--- vendor/Game2D/AbstractGameObject/AbstractGameObject.ts
+++ vendor/Game2D/AbstractGameObject/AbstractGameObject.ts
@@ -1,12 +1,14 @@
 import AbstractGeometries2D from '@Vendor/Graphics2D/Geometries2D/AbstractGeometries2D'
 import InterfaceAbstractGameObject from './InterfaceAbstractGameObject'
 import AbstractGame from '../AbstractGame/AbstractGame'
+import BoundingBox from '../BoundingBox'
 
 export default abstract class AbstractGameObject
   implements InterfaceAbstractGameObject
 {
   index: number = 0
   abstract sprite: AbstractGeometries2D
+  abstract boundingBox: BoundingBox<AbstractGeometries2D> | null
 
   constructor(readonly game: AbstractGame) {}
 
--- vendor/Game2D/AbstractGameObject/InterfaceAbstractGameObject.d.ts
+++ vendor/Game2D/AbstractGameObject/InterfaceAbstractGameObject.d.ts
@@ -1,10 +1,12 @@
 import AbstractGeometries2D from '@Vendor/Graphics2D/Geometries2D/AbstractGeometries2D'
 import AbstractGame from '../AbstractGame/AbstractGame'
+import BoundingBox from '../BoundingBox'
 
 export default interface InterfaceAbstractGameObject {
   index: number
   sprite: AbstractGeometries2D
   game: AbstractGame
+  boundingBox: BoundingBox<AbstractGeometries2D> | null
 
   init(): void
   update(): void
