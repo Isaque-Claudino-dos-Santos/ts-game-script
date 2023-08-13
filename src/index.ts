52090ff33e2a1618079daf9cae71fde3311357bb
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-08-13 00:31:27 -0300

    feat: instance game

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import Engine2D from '@Vendor/Engine2D'
+import GameMyGame from './GameMyGame'
 
-// const engine = new Engine2D()
+const engine = new Engine2D()
+const game = new GameMyGame(engine.canvas)
+
+engine.initialize(game, () => {
+  console.log('Initialized Game!')
+})
