982a394dbde60f1d0b3dfd7195dc24183f2d3092
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-08-13 02:07:16 -0300

    feat: instancied collider2d

--- vendor/Game2D/AbstractScene/AbstractScene.ts
+++ vendor/Game2D/AbstractScene/AbstractScene.ts
@@ -2,9 +2,11 @@ import objFor from '@Vendor/utils/objFor'
 import AbstractGame from '../AbstractGame'
 import InterfaceAbstractScene, { SceneObjects } from './InterfaceAbstractScene'
 import AbstractGameObject from '../AbstractGameObject'
+import Collider2D from '../Collider2D'
 
 export default abstract class AbstractScene implements InterfaceAbstractScene {
   abstract objects: SceneObjects
+  readonly collider: Collider2D = new Collider2D()
 
   constructor(readonly game: AbstractGame) {}
 
--- vendor/Game2D/AbstractScene/InterfaceAbstractScene.d.ts
+++ vendor/Game2D/AbstractScene/InterfaceAbstractScene.d.ts
@@ -1,11 +1,13 @@
 import AbstractGame from '../AbstractGame'
 import AbstractGameObject from '../AbstractGameObject'
+import Collider2D from '../Collider2D'
 
 export type SceneObjects = { [index: string]: AbstractGameObject }
 
 export default interface InterfaceAbstractScene {
   readonly game: AbstractGame
   readonly objects: SceneObjects
+  readonly collider: Collider2D
 
   callInitInObjects(): void
   callUpdateInObjects(): void
