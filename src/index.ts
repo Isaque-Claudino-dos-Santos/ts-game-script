be678207849373f00e23b38df792ace231b618c5
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-08-27 00:03:30 -0300

    feat: configured canvas

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,18 @@ import GameMyGame from './GameMyGame'
 const game = new GameMyGame()
 const engine = new Engine2D(game)
 
+/*
+##################
+###Canvas Setup###
+##################
+*/
+game.canvas.screen.width = 550 / 2 // resolution width
+game.canvas.screen.height = 400 / 2 // resolution height
+game.canvas.screen.style.imageRendering = 'pixelated' // pixelated to images
+game.canvas.screen.style.width = '550px' // canvas width
+game.canvas.screen.style.height = '400px' // canvas height
+
+// Boot engine
 engine.initialize(() => {
   console.log('Initialized Game!')
 })
