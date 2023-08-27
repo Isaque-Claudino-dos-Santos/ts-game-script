474b49806f933b7c49db1c9923c7cf52a55dec3a
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-08-27 17:54:15 -0300

    feat: removed prop

--- vendor/Game2D/AbstractGame/InterfaceAbstractGame.d.ts
+++ vendor/Game2D/AbstractGame/InterfaceAbstractGame.d.ts
@@ -2,14 +2,12 @@ import Mouse from '@Vendor/Events/Mouse'
 import HandlerScenes from '../HandlerScenes'
 import Keyboard from '@Vendor/Events/Keyboard'
 import Canvas2D from '@Vendor/Graphics2D/Canvas2D'
-import Timer from '@Vendor/Events/Timer'
 
 export default interface InterfaceAbstractGame {
   readonly scenes: HandlerScenes
   readonly canvas: Canvas2D
   readonly mouse: Mouse
   readonly keyboard: Keyboard
-  readonly timer: Timer
 
   gameInit(): void
   gameUpdate(): void
