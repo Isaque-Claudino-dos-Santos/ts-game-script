1c5d97ce7e84fe972f51e0203b87d93af0f29e04
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-08-26 22:21:59 -0300

    feat: removed optional boundingbox

--- vendor/Game2D/AbstractGameObject/AbstractGameObject.ts
+++ vendor/Game2D/AbstractGameObject/AbstractGameObject.ts
@@ -9,7 +9,7 @@ export default abstract class AbstractGameObject
 {
   index: number = 0
   abstract sprite: AbstractGeometries2D
-  abstract boundingBox: BoundingBox<AbstractGeometries2D> | null
+  abstract boundingBox: BoundingBox<AbstractGeometries2D>
   readonly collider: Collider2D = new Collider2D(this.game.scenes, this)
 
   constructor(readonly name: string, readonly game: AbstractGame) {}
