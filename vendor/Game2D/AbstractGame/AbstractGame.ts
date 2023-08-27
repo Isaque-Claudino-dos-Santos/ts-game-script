c977ac008d790a8a3a372164994bb3516f445b15
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-08-27 19:13:20 -0300

    feat: add time and setup time

--- vendor/Game2D/AbstractGame/AbstractGame.ts
+++ vendor/Game2D/AbstractGame/AbstractGame.ts
@@ -3,12 +3,14 @@ import Mouse from '@Vendor/Events/Mouse'
 import HandlerScenes from '../HandlerScenes'
 import InterfaceAbstractGame from './InterfaceAbstractGame'
 import Canvas2D from '@Vendor/Graphics2D/Canvas2D'
+import GameTime from '../GameTime'
 
 export default abstract class AbstractGame implements InterfaceAbstractGame {
   readonly canvas: Canvas2D = new Canvas2D()
   readonly mouse: Mouse = new Mouse(this.canvas.screen)
   readonly keyboard: Keyboard = new Keyboard()
   readonly scenes: HandlerScenes = new HandlerScenes(this)
+  readonly time: GameTime = new GameTime()
 
   beforeInit() {}
   afterInit() {}
@@ -24,6 +26,11 @@ export default abstract class AbstractGame implements InterfaceAbstractGame {
   }
 
   gameUpdate = () => {
+    this.time.prevTime = this.time.currentTime
+    this.time.currentTime = new Date().getMilliseconds()
+    this.time.deltaTime = this.time.currentTime - this.time.prevTime
+    this.time.totalFrames++
+
     this.beforeUpdate()
     this.scenes.current().callUpdateInObjects()
     this.afterUpdate()
--- vendor/Game2D/AbstractGame/InterfaceAbstractGame.d.ts
+++ vendor/Game2D/AbstractGame/InterfaceAbstractGame.d.ts
@@ -2,12 +2,21 @@ import Mouse from '@Vendor/Events/Mouse'
 import HandlerScenes from '../HandlerScenes'
 import Keyboard from '@Vendor/Events/Keyboard'
 import Canvas2D from '@Vendor/Graphics2D/Canvas2D'
+import GameTime from '../GameTime'
 
 export default interface InterfaceAbstractGame {
   readonly scenes: HandlerScenes
   readonly canvas: Canvas2D
   readonly mouse: Mouse
   readonly keyboard: Keyboard
+  readonly time: GameTime
+
+  beforeInit(): void
+  afterInit(): void
+  beforeUpdate(): void
+  afterUpdate(): void
+  beforeRender(): void
+  afterRender(): void
 
   gameInit(): void
   gameUpdate(): void
