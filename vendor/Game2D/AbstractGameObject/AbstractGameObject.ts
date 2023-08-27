164ae75907b730d840c18a95981afa105a5afd04
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-08-26 22:14:10 -0300

    feat: add attribute collider2d

--- vendor/Game2D/AbstractGameObject/AbstractGameObject.ts
+++ vendor/Game2D/AbstractGameObject/AbstractGameObject.ts
@@ -2,6 +2,7 @@ import AbstractGeometries2D from '@Vendor/Graphics2D/Geometries2D/AbstractGeomet
 import InterfaceAbstractGameObject from './InterfaceAbstractGameObject'
 import AbstractGame from '../AbstractGame/AbstractGame'
 import BoundingBox from '../BoundingBox'
+import Collider2D from '../Collider2D'
 
 export default abstract class AbstractGameObject
   implements InterfaceAbstractGameObject
@@ -9,6 +10,7 @@ export default abstract class AbstractGameObject
   index: number = 0
   abstract sprite: AbstractGeometries2D
   abstract boundingBox: BoundingBox<AbstractGeometries2D> | null
+  readonly collider: Collider2D = new Collider2D(this.game.scenes, this)
 
   constructor(readonly name: string, readonly game: AbstractGame) {}
 
--- vendor/Game2D/AbstractGameObject/InterfaceAbstractGameObject.d.ts
+++ vendor/Game2D/AbstractGameObject/InterfaceAbstractGameObject.d.ts
@@ -2,12 +2,14 @@ import AbstractGeometries2D from '@Vendor/Graphics2D/Geometries2D/AbstractGeomet
 import AbstractGame from '../AbstractGame/AbstractGame'
 import BoundingBox from '../BoundingBox'
 import AbstractGameObject from './AbstractGameObject'
+import Collider2D from '../Collider2D'
 
 export default interface InterfaceAbstractGameObject {
   index: number
   sprite: AbstractGeometries2D
   game: AbstractGame
   boundingBox: BoundingBox<AbstractGeometries2D> | null
+  collider: Collider2D
   name: string
 
   init(): void
