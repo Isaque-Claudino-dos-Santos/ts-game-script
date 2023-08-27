2c3bca5c6a5cc388d77657c72f89815f6adee73d
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-08-26 23:49:26 -0300

    feat: reposition box

--- src/MainScene.ts
+++ src/MainScene.ts
@@ -6,8 +6,8 @@ export default class SceneMain extends AbstractScene {
   init = () => {
     this.add('player', ObjectPlayer, 'moving')
     const b1 = this.add('box1', ObjectBox)
-    b1.sprite.x(100).y(30).width(20).height(30)
+    b1.sprite.x(100).y(30).width(30).height(30)
     const b2 = this.add('box2', ObjectBox)
-    b2.sprite.x(10).y(100)
+    b2.sprite.x(100).y(80).width(30).height(30)
   }
 }
