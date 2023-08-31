bd82b7ca67fbd0f721b8245432624b00162af0af
Isaque dos Santos <74436802+Isaque-Claudino-dos-Santos@users.noreply.github.com>
2023-08-31 22:25:01 +0000

    Update GameMyGame.ts

--- src/GameMyGame.ts
+++ src/GameMyGame.ts
@@ -1,6 +1,6 @@
 import AbstractGame from '@Vendor/Game2D/AbstractGame'
 import SceneMain from './MainScene'
-
+ 
 export default class GameMyGame extends AbstractGame {
   override beforeInit(): void {
     this.scenes.add('main', SceneMain)
