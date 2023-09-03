3536394d5ec67d58663bc44c6d2b27a00fdbe1f2
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-09-03 20:59:25 -0300

    feat: removed props and add methods

--- vendor/Game2D/GameTime/InterfaceGameTime.d.ts
+++ vendor/Game2D/GameTime/InterfaceGameTime.d.ts
@@ -1,7 +1,7 @@
 export default interface InterfaceGameTime {
-  currentTime: number
-  prevTime: number
   deltaTime: number
   totalFrames: number
-  frameRate: number
+  totalTime: number
+
+  handle(): void
 }
