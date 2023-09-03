47ddfc8a07bf72320777102f295ada98f57ee7c8
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-09-03 20:59:13 -0300

    feature: created handle method and implemented

--- vendor/Game2D/GameTime/GameTime.ts
+++ vendor/Game2D/GameTime/GameTime.ts
@@ -1,9 +1,20 @@
 import InterfaceGameTime from './InterfaceGameTime'
 
 export default class GameTime implements InterfaceGameTime {
-  currentTime: number = 0
-  prevTime: number = 0
+  private currentTime: number = 0
+  private prevTime: number = 0
   totalFrames: number = 0
-  frameRate: number = 0
   deltaTime: number = 0
+  totalTime: number = 0
+
+  handle(): void {
+    this.prevTime = this.currentTime
+    this.currentTime = new Date().getMilliseconds()
+
+    if (this.prevTime > this.currentTime) this.prevTime -= 1000
+
+    this.deltaTime = this.currentTime - this.prevTime
+    this.totalFrames++
+    this.totalTime += this.deltaTime
+  }
 }
