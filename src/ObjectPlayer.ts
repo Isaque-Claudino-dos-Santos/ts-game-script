69eb63b593dcc3b64804adfe46d0373a9af3bd91
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-09-03 21:34:28 -0300

    feature: timer to color

--- src/ObjectPlayer.ts
+++ src/ObjectPlayer.ts
@@ -2,10 +2,13 @@ import AbstractGameObject from '@Vendor/Game2D/AbstractGameObject'
 import Rect from '@Vendor/Graphics2D/Geometries2D/Rect'
 import BoundingBox from '@Vendor/Game2D/BoundingBox'
 import ObjectBox from './ObjectBox'
+import Timer from '@Vendor/Events/Timer'
 
 export default class ObjectPlayer extends AbstractGameObject {
   readonly sprite = new Rect()
   readonly boundingBox: BoundingBox<Rect> = new BoundingBox(this, new Rect())
+  readonly timeColor = new Timer(this.game)
+
   speedX = 0
   speedY = 0
   mvX = 0
@@ -14,14 +17,22 @@ export default class ObjectPlayer extends AbstractGameObject {
   checkTime() {
     const key = this.game.keyboard
 
+    if (this.timeColor.onTime(0.8)) {
+      const factoryRandom = () => Math.floor(Math.random() * 9)
+      this.sprite.bgColor(
+        `#${factoryRandom()}${factoryRandom()}${factoryRandom()}${factoryRandom()}${factoryRandom()}${factoryRandom()}`
+      )
+    }
+
     if (key.check('Space')) {
+      this.timeColor.toggle()
       key.lockKey('Space')
     }
   }
 
   moviment() {
-    this.speedX = 10
-    this.speedY = 10
+    this.speedX = 3
+    this.speedY = 3
     const key = this.game.keyboard
     this.mvX = key.check('ArrowRight') - key.check('ArrowLeft')
     this.mvY = key.check('ArrowDown') - key.check('ArrowUp')
