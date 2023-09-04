4078c8d764ff5871a2fd8357293134b8fb019e9e
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-09-03 23:35:07 -0300

    refectory: refectored method

--- vendor/Game2D/Collider2D/Collider2D.ts
+++ vendor/Game2D/Collider2D/Collider2D.ts
@@ -20,39 +20,29 @@ export default class Collider2D implements InterfaceCollider2D {
     const objBox = this.object.boundingBox.box
 
     if (!is.undefined(className)) {
-      let collided: boolean = false
       const boxies = this.scenes
         .current()
         .getObjectsStaticByClassName(className)
         .map((o) => o.boundingBox.box)
 
-      boxies.forEach((box) => {
-        let catX = objBox.originX() + x
-        let catY = objBox.originY() + y
+      for (const box of boxies) {
+        if (box instanceof Rect && objBox instanceof Rect) {
+          let catX = box.x() + box.halfWidth() - (objBox.originX() + x)
+          let catY = box.y() + box.halfHeight() - (objBox.originY() + y)
 
-        if (box instanceof Rect) {
-          catX -= box.x() + box.halfWidth()
-          catY -= box.y() + box.halfHeight()
-          if (objBox instanceof Rect) {
-            const sumWidth = objBox.halfWidth() + box.halfWidth()
-            const sumHeight = objBox.halfHeight() + box.halfHeight()
+          const sumWidth = objBox.halfWidth() + box.halfWidth()
+          const sumHeight = objBox.halfHeight() + box.halfHeight()
 
-            if (sumWidth > Math.abs(catX) && sumHeight > Math.abs(catY)) {
-              const overlapX = sumWidth + catX
-              const overlapY = sumHeight + catY
+          if (sumWidth > Math.abs(catX) && sumHeight > Math.abs(catY)) {
+            const overlapX = sumWidth + catX
+            const overlapY = sumHeight + catY
 
-              if (overlapX > 0) return (collided = true)
-              if (overlapY > 0) return (collided = true)
-
-              collided = false
-            }
+            return overlapX > 0 && overlapY > 0
           }
         }
-      })
-
-      return collided
+      }
     }
 
-    return true
+    return false
   }
 }
